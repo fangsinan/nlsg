f9d448159927918102507e67101bbea1da55a2e6
zouxuguang <>
2020-06-04 10:40:11 +0800

    no message

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::namespace('Api')->group(function(){
+Route::namespace('Api\V4')->group(function(){
    Route::get('index', 'IndexController@index');
 
 });
