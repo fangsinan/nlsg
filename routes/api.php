8fa1f8c109dbf4450e1331b2e5a68f07495e903d
fangsinan <15650701817@163.com>
2020-06-10 16:26:18 +0800

    add

--- routes/api.php
+++ routes/api.php
@@ -29,18 +29,26 @@ Route::namespace('Api\V4')->group(function() {
 
     //专栏
     Route::get('/v4/column/get_column_list', 'ColumnController@getColumnList');
-    Route::get('/v4/column/get_column_detail', 'WorksController@getColumnDetail');
+    Route::get('/v4/column/get_column_detail', 'ColumnController@getColumnDetail');
     Route::get('/v4/column/get_column_works', 'ColumnController@getColumnWorks');
+    Route::get('/v4/column/collection', 'ColumnController@Collection');
+    Route::get('/v4/column/get_recommend', 'ColumnController@getRecommend');
 
 
     //课程
     Route::get('/v4/works/get_works_detail', 'WorksController@getWorksDetail');
     Route::get('/v4/works/show', 'WorksController@show');
     Route::get('/v4/works/edit_history_time', 'WorksController@editHistoryTime');
-    Route::get('/v4/works/works_collection', 'WorksController@worksCollection');
-    Route::get('/v4/works/get_works_category', 'WorksController@getWorksCategory');
+    Route::get('/v4/works/get_works_category', 'WorksController@getWorksCategoryTeacher');
+    Route::get('/v4/works/get_works_content', 'WorksController@getWorksContent');
+    Route::get('/v4/works/get_works_index', 'WorksController@getWorksIndex');
 
+    //下单
+    Route::get('/v4/order/create_column_order', 'OrderController@createColumnOrder');
+    Route::get('/v4/order/create_works_order', 'OrderController@createWorksOrder');
+    Route::get('/v4/order/get_coupon', 'OrderController@getCoupon');
 
+    
     //商城部分
     Route::get('/V4/goods/info', 'MallController@goodsList');
     Route::get('/V4/goods/coupon_list', 'MallController@couponList');
