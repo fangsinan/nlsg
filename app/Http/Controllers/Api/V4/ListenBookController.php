63001ae66deb519ac2bdc6be58e6e1df6ade0bd3
fangsinan <15650701817@163.com>
2020-06-18 14:38:59 +0800

    coupon

--- app/Http/Controllers/Api/V4/ListenBookController.php
+++ app/Http/Controllers/Api/V4/ListenBookController.php
@@ -5,86 +5,86 @@ namespace App\Http\Controllers\Api\V4;
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Lists;
+use App\Models\ListsWork;
+use App\Models\Subscribe;
+use App\Models\Works;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
-class ListenBook extends Controller
+class ListenBookController extends Controller
 {
+    //  听书首页
+    public function ListenBookIndex(){
+        //主编力荐
+
+        //热门推荐
+
+        //精选书单
+
+        //新书速递
+
+        //热门分类
+
+        //猜你喜欢
+
+    }
+
 
     /**
-     * @api {post} /api/v4/column/get_book_list 听书-精选书单
-     * @apiName get_column_list
+     * @api {post} /api/v4/book/get_book_list 听书-精选书单
+     * @apiName get_book_list
      * @apiVersion 1.0.0
-     * @apiGroup Column
+     * @apiGroup book
      *
-     * @apiParam {int} page  （非必填）
-     * @apiParam {int} pageSize  (非必填）
-     * @apiParam {int} order 1默认倒序 2正序
+     * @apiParam {int} page
      *
      * @apiSuccess {string} result json
      * @apiSuccessExample Success-Response:
-     * {
-    "code": 200,
-    "msg": "成功",
-    "data": [
-    {
-    "id": 1,
-    "name": "王琨专栏",
-    "type": 1,
-    "user_id": 211172,
-    "message": "",
-    "original_price": "0.00",
-    "price": "0.00",
-    "online_time": 0,
-    "works_update_time": 0,
-    "cover_pic": "/wechat/works/video/161627/2017121117503851065.jpg",
-    "details_pic": ""
-    },
-    {
-    "id": 2,
-    "name": "张宝萍专栏",
-    "type": 1,
-    "user_id": 1,
-    "message": "",
-    "original_price": "0.00",
-    "price": "0.00",
-    "online_time": 0,
-    "works_update_time": 0,
-    "cover_pic": "/wechat/works/video/161627/2017121117503851065.jpg",
-    "details_pic": ""
-    },
-    {
-    "id": 3,
-    "name": "王复燕专栏",
-    "type": 1,
-    "user_id": 211171,
-    "message": "",
-    "original_price": "0.00",
-    "price": "0.00",
-    "online_time": 0,
-    "works_update_time": 0,
-    "cover_pic": "/wechat/works/video/161627/2017121117503851065.jpg",
-    "details_pic": ""
-    }
-    ]
-    }
      */
-    public function getColumnList(Request $request){
-        //排序
-        $order = $request->input('order',1);
-        //type 1 专栏  2讲座
-        $type   = $request->input('type',1);
-        $order_str = 'asc';
-        if($order){
-            $order_str = 'desc';
+    public function getBookList(Request $request){
+
+        $list = Lists::select(['id', 'title', 'subtitle', 'cover', ])->where([
+            'status'=>1
+            ])->paginate($this->page_per_page)->toArray();
+
+        foreach ($list['data'] as $key => &$val){
+            $val['lists_count'] = ListsWork::where(['lists_id'=>$val['id']])->count();
         }
-        $field = ['id', 'name', 'column_type', 'user_id', 'message', 'original_price', 'price', 'online_time', 'works_update_time', 'cover_pic', 'details_pic'];
-        $list = Column::where([
-            "status" => 1,
-            "type"   => $type,
-        ])->orderBy('updated_at', 'desc')
-            ->orderBy('sort', $order_str)->get($field);
         return $this->success($list);
     }
 
 
+
+    /**
+     * @api {post} /api/v4/book/get_book_list_detail 听书-精选书单详情
+     * @apiName get_book_list_detail
+     * @apiVersion 1.0.0
+     * @apiGroup book
+     *
+     * @apiParam {int} lists_id 书单id
+     * @apiParam {int} page
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+     */
+    public function getBookListDetail(Request $request){
+        $lists_id = $request->input('lists_id',0);
+        $user_id = $request->input('user_id',0);
+
+        $lists = ListsWork::select('*')->where(['lists_id'=>$lists_id])->paginate($this->page_per_page)->toArray();
+        $works_ids = array_column($lists['data'],'works_id');
+        //$works_ids = array_column($lists,'works_id');
+
+        $works = Works::select(['id','type', 'title', 'subtitle', 'cover_img','original_price','price', 'message','is_free'])
+            ->where(['status' => 4 , 'is_audio_book' => 1,])
+            ->whereIn('id', $works_ids)->get()->toArray();
+
+        foreach ($works as $key=>$val){
+            //是否购买
+            $works[$key]['is_sub'] = Subscribe::isSubscribe($user_id, $val['id'], 2);
+        }
+
+        return $this->success($works);
+    }
 }
