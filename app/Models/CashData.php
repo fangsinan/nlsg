5d40917bf0c212bcd98be4011d3c86be35ec0a83
fangsinan <15650701817@163.com>
2020-07-08 18:45:22 +0800

    model

--- app/Models/CashData.php
+++ app/Models/CashData.php
@@ -3,9 +3,11 @@
 
 namespace App\Models;
 
-
 class CashData extends Base
 {
     protected $table = 'nlsg_cash_data';
+
+    protected $fillable = ['user_id' , 'idcard' , 'tax_deduction' , 'truename', 'idcard_cover'  , 'zfb_account' , 'phone'  , 'reason' , 'default_account'  , 'is_pass'  , 'wx_account', 'WxNickName', 'app_wx_account', 'app_WxNickName', 'balance2017_cash_time',
+        'type', 'org_type', 'org_name', 'org_area', 'org_address', 'org_license_picture', 'bank_opening', 'bank_number', 'bank_permit_picture', 'log',];
 
 }
