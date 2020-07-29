fa5890e73e57f6ed2c94e1e402ef784e49c4b4ea
wangxh <>
2020-07-29 14:41:40 +0800

    no message

--- app/Http/Controllers/Admin/V4/GoodsController.php
+++ app/Http/Controllers/Admin/V4/GoodsController.php
@@ -2,19 +2,23 @@
 
 
 namespace App\Http\Controllers\Admin\V4;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\servers\MallOrderServers;
+use App\servers\GoodsServers;
 
 class GoodsController extends Controller
 {
     //todo 添加商品
-    public function add(Request $request){
-
-
-
-        $params = $request->input();
-
-        return $this->success($params);
+    public function add(Request $request)
+    {
+        $servers = new GoodsServers();
+        $data = $servers->add($request->input());
+        if (($data['code'] ?? true) === false) {
+            $ps = ($this->show_ps ? (($data['ps'] ?? false) ? (':' . $data['ps']) : '') : '');
+            return $this->error(0, $data['msg'] . $ps);
+        } else {
+            return $this->success($data);
+        }
     }
 }
--- app/Servers/GoodsServers.php
+++ app/Servers/GoodsServers.php
@@ -4,7 +4,41 @@
 namespace App\Servers;
 
 
+use App\Models\MallGoods;
+use Illuminate\Support\Facades\DB;
+
 class GoodsServers
 {
+    public function add($params)
+    {
+        if ($params['goods_id'] ?? 0) {
+            $goods_model = MallGoods::find($params['goods_id']);
+            if (!$goods_model) {
+                return ['code' => false, 'msg' => 'id错误'];
+            }
 
+        } else {
+            $goods_model = new MallGoods();
+        }
+
+        if (empty($params['category_id'])) {
+            return ['code' => false, 'msg' => '参数错误:category_id'];
+        } else {
+            $goods_model->category_id = 1;
+        }
+
+        $goods_model->name = "手机紧身的繁了";
+        $goods_model->subtitle = "一个还行的手机";
+        $goods_model->picture = "/phone/1.jpg";
+        $goods_model->freight_id = 1;
+        $goods_model->original_price = 9999;
+        $goods_model->price = 999;
+        $goods_model->keywords = "手机,智能,安卓";
+        $goods_model->content = "<p>图文简介啊发撒发撒地方</p>";
+        $goods_model->status = 1;
+
+        DB::beginTransaction();
+
+
+    }
 }
