a5a86a055c2e4cc39ba22fca29e3780c8b55cebe
thinkphper <thinkphper@gmail.com>
2020-09-15 17:08:12 +0800

    修改

--- app/Http/Controllers/Api/V4/UserController.php
+++ app/Http/Controllers/Api/V4/UserController.php
@@ -893,8 +893,10 @@ class UserController extends Controller
     {
         $uid   = $this->user['id'];
         $lists = User::select('id','nickname','headimg','phone','is_author','notify_num','follow_num','fan_num','history_num')
-                ->find($uid)
-                ->toArray();
+                ->first($uid);
+        if ($lists) {
+            $lists->phone = substr_replace($user->phone, '****', 3, 4);
+        }
         return success($lists);
     }
 
