c811096a2dcce036f39ec5c440ffe7c87a1c24d5
wangxh <>
2020-10-30 11:03:26 +0800

    no message

--- app/Models/LiveNotice.php
+++ app/Models/LiveNotice.php
@@ -143,12 +143,12 @@ class LiveNotice extends Base
         }
 
         if (empty($params['id'] ?? 0)) {
-            $query = new self();
+            $query = self::query();
         } else {
             $query = self::whereId($params['id']);
         }
 
-        $query = $query->where('live_id', '=', $live_id)
+        $query->where('live_id', '=', $live_id)
             ->where('live_info_id', '=', $live_info_id)
             ->where('type', '=', $type)
             ->where('is_del', '=', 0)
@@ -161,10 +161,10 @@ class LiveNotice extends Base
 
         switch ($params['ob'] ?? '') {
             case 't_asc':
-                $query = $query->orderBy('id', 'asc');
+                $query->orderBy('id', 'asc');
                 break;
             default:
-                $query = $query->orderBy('id', 'desc');
+                $query->orderBy('id', 'desc');
         }
 
         $list = $query->limit($size)
