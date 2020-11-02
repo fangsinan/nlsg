67420f92aa64002523ea45e499eb9dcefa3e9afb
thinkphper <thinkphper@gmail.com>
2020-11-02 10:31:57 +0800

    修改

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -13,29 +13,20 @@ class Live extends Model
         if ( ! $ids) {
             return false;
         }
-        $lists = $this->select('id', 'title', 'describe', 'cover_img', 'begin_at', 'end_at','price','order_num')
+        $list = $this->select('id', 'title', 'describe', 'cover_img', 'begin_at', 'end_at','price','order_num')
             ->whereIn('id', $ids)
             ->where('is_del', 0)
             ->orderBy('created_at', 'desc')
             ->first();
-//        if ($lists) {
-//            foreach ($lists as &$v) {
-//                $channel = LiveInfo::where('live_pid', $v['id'])
-//                            ->where('status', 1)
-//                            ->orderBy('id','desc')
-//                            ->first();
-//                if (strtotime($channel['begin_at']) > time()) {
-//                   $v['live_status'] = '1';
-//                } else {
-//                   if (strtotime($channel['end_at']) < time()) {
-//                       $v['live_status'] = '2';
-//                   } else {
-//                       $v['live_status'] = '3';
-//                   }
-//                }
-//            }
-//        }
-        return $lists;
+
+        if ($list) {
+            $channel = LiveInfo::where('live_pid', $list->id)
+                        ->where('status', 1)
+                        ->orderBy('id','desc')
+                        ->first();
+            $list['info_id'] =  $channel->id;
+        }
+        return $list;
     }
 
     public function getLiveLists()
