75eec5080c1677919a85ab9329cdda6560f5d60f
wangxh <>
2020-11-02 17:28:21 +0800

    no message

--- app/Models/MallOrderGroupBuy.php
+++ app/Models/MallOrderGroupBuy.php
@@ -929,6 +929,13 @@ class MallOrderGroupBuy extends Base
         foreach ($price_list_new as $new_k => $new_v) {
             if ($new_v['value'] == 0) {
                 unset($price_list_new[$new_k]);
+            } else {
+                if (in_array($new_v['key'], ['权益立减', '活动立减', '优惠券总额'])) {
+                    $new_v['value'] = '-' . $new_v['value'];
+                }
+                if (in_array($new_v['key'], ['运费'])) {
+                    $new_v['value'] = '+' . $new_v['value'];
+                }
             }
         }
         $price_list_new = array_values($price_list_new);
