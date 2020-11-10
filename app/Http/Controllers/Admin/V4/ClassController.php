acb78c7bbe9844b99f6aaeab2ec6703844c777d5
thinkphper <thinkphper@gmail.com>
2020-11-10 14:13:07 +0800

    修改专栏注释

--- app/Http/Controllers/Admin/V4/ClassController.php
+++ app/Http/Controllers/Admin/V4/ClassController.php
@@ -29,6 +29,15 @@ class ClassController extends Controller
      * @apiParam {string} start  开始时间
      * @apiParam {string} end    结束时间
      *
+     *  @apiSuccess {string} name  专栏名称
+      * @apiSuccess {string} title  标题
+      * @apiSuccess {string} subtitle  副标题
+      * @apiSuccess {string} user    作者相关
+      * @apiSuccess {string}  作品数量 待定
+      * @apiSuccess {string}  price  价格
+      * @apiSuccess {number}  status  状态
+      * @apiSuccess {string}  created_at  创建时间
+     *
      * @apiSuccessExample  Success-Response:
      * HTTP/1.1 200 OK
      * {
@@ -65,7 +74,7 @@ class ClassController extends Controller
                 ]);
             });
 
-        $lists = $query->select('id', 'user_id', 'name', 'title', 'subtitle', 'price')
+        $lists = $query->select('id', 'user_id', 'name', 'title', 'subtitle', 'price','status','created_at')
             ->where('type', 1)
             ->orderBy('created_at', 'desc')
             ->paginate(10)
