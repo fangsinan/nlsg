c73bcb7385615f6c0091df7a0e108cde27b5dd58
fangsinan <15650701817@163.com>
2020-11-12 19:27:06 +0800

    no message

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -81,6 +81,9 @@ class Order extends Base
                 $model = new Column();
                 $result = $model->getIndexColumn([$relation_id]);
                 break;
+            case 16:
+                $result = ['id'=>1,'type' => 6, 'text'=>'幸福360会员','img'=>'/nlsg/poster_img/1581599882211_.pic.jpg','price'=>360.00];
+                break;
             case 17:
                 if($send_type == 1 || $send_type == 2){
                     $model = new Column();
