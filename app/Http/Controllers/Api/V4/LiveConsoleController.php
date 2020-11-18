2c85e7627a9d8739f70b383a74fc212f4c7b572d
wangxh <>
2020-11-18 20:06:10 +0800

    no message

--- app/Http/Controllers/Api/V4/LiveConsoleController.php
+++ app/Http/Controllers/Api/V4/LiveConsoleController.php
@@ -74,6 +74,7 @@ class LiveConsoleController extends Controller
     public function add(Request $request)
     {
         $params = $request->input();
+        return $this->getRes($params);
         $model = new LiveConsole();
         $data = $model->add($params, $this->user['id']);
         return $this->getRes($data);
