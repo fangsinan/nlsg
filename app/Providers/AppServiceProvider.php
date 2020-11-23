12c069bb06b256433e1ffd616ae9711a1b707ed2
thinkphper <thinkphper@gmail.com>
2020-11-23 16:13:56 +0800

    修改

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,13 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-       DB::listen(function($query) {
-           \Log::info(
-               $query->sql,
-               $query->bindings,
-               $query->time
-           );
-       });
+       // DB::listen(function($query) {
+       //     \Log::info(
+       //         $query->sql,
+       //         $query->bindings,
+       //         $query->time
+       //     );
+       // });
     }
 
     /**
