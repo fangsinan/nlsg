4cf15edbd8c8a83f123c1e8293e1d3ad209cf5f8
fangsinan <15650701817@163.com>
2020-12-04 15:53:27 +0800

    no message

--- app/Http/Controllers/Api/V4/WorksController.php
+++ app/Http/Controllers/Api/V4/WorksController.php
@@ -254,8 +254,7 @@ class WorksController extends Controller
         $worksData = DB::table($relationObj->getTable(), ' relation')
             ->leftJoin($worksObj->getTable() . ' as works', 'works.id', '=', 'relation.work_id')
             ->select('works.id', 'works.type', 'works.title', 'works.user_id', 'works.cover_img', 'works.price', 'works.original_price', 'works.subtitle',
-                'works.works_update_time','works.detail_img','works.content','relation.id as relation_id','relation.category_id','relation.work_id',
-                'works.column_id')
+                'works.works_update_time','works.detail_img','works.content','relation.id as relation_id','relation.category_id','relation.work_id', 'works.column_id')
 //            ->select('works.*')
             ->where($where)
             ->orderBy('works.'.$order_str,'desc')
