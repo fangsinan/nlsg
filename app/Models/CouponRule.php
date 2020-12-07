fb339af115dddd944204301c0238970de9d4b627
wangxh <>
2020-12-07 11:30:31 +0800

    商品优惠券列表

--- app/Models/CouponRule.php
+++ app/Models/CouponRule.php
@@ -49,13 +49,12 @@ class CouponRule extends Base
             }
         }
 
-        //todo 修改,查询库存
         //只返回有库存的
         foreach ($res as $k => $v) {
             //infinite库存无限  1无限  0有限
             if ($v->infinite == 0 && ($v->stock - $v->used_stock <= 0)) {
                 $v->can_use = 0;
-                if (($params['only_stock'] ?? 0) == 1) {
+                if (($params['show_zero_stock'] ?? 0) == 0) {
                     unset($res[$k]);
                 }
             }
