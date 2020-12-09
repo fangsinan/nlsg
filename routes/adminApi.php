3598fbd4a3b9e35a48ad8c9aa8dba8b4e5cdcb20
thinkphper <thinkphper@gmail.com>
2020-12-09 15:07:44 +0800

    修改

--- routes/adminApi.php
+++ routes/adminApi.php
@@ -20,6 +20,7 @@ Route::group(['namespace' => 'Admin\V4', 'prefix' => 'admin_v4'], function () {
     Route::get('class/column', 'ClassController@column');
     Route::get('class/get-column-list', 'ClassController@getColumnList');
     Route::get('class/get-column-work-list', 'ClassController@getColumnWorkList');
+    Route::get('class/get-lecture-work-list', 'ClassController@getLectureWorkList');
     Route::get('class/get-work-chapter-list', 'ClassController@getWorkChapterList');
     Route::get('class/lecture', 'ClassController@lecture');
     Route::get('class/works', 'ClassController@works');
