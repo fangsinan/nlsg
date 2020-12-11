9a6aeace209fe605e147717ecfd1375bc0ea5c9c
thinkphper <thinkphper@gmail.com>
2020-12-11 11:22:30 +0800

    增加教育宝典

--- app/Http/Controllers/Admin/V4/IndexController.php
+++ app/Http/Controllers/Admin/V4/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\V4;
 
 use App\Http\Controllers\Controller;
+use App\Models\Lists;
 use App\Models\Recommend;
 use App\Models\Works;
 use Illuminate\Http\Request;
@@ -41,17 +42,68 @@ class IndexController extends Controller
             ->where('type', 2)
             ->value('relation_id');
         $ids = explode(',', $list);
-        if (!$ids){
-            return error(1000,'还没有推荐');
+        if (!$ids) {
+            return error(1000, '还没有推荐');
         }
 
-        $works = Works::select('id', 'column_id', 'type', 'user_id', 'title', 'cover_img', 'subtitle', 'price', 'is_free','status')
+        $works = Works::select('id', 'column_id', 'type', 'user_id', 'title', 'cover_img', 'subtitle', 'price', 'is_free', 'status')
             ->whereIn('id', $ids)
             ->where('status', 4)
             ->orderBy('created_at', 'desc')
             ->get()
             ->toArray();
         return success($works);
+    }
+
+    /**
+     * @api {get} api/v4/index/course  首页-课程集合
+     * @apiVersion 4.0.0
+     * @apiName  course
+     * @apiGroup Index
+     * @apiSampleRequest http://app.v4.api.nlsgapp.com/api/v4/index/course
+     *
+     * @apiSuccess {string} title 标题
+     * @apiSuccess {string} subtitle 副标题
+     * @apiSuccess {string} cover 封面
+     * @apiSuccess {number}  num  数量
+     * @apiSuccess {string}  works 听书作品
+     * @apiSuccess {string}  works.works_id  作品id
+     * @apiSuccess {string}  works.title  作品标题
+     * @apiSuccess {string}  works.cover_img  作品封面
+     *
+     * @apiSuccessExample  Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "code": 200,
+     *       "msg" : '成功',
+     *       "data":[
+     *         ]
+     *     }
+     *
+     */
+    public function course()
+    {
+        $list = Recommend::where('position', 1)
+            ->where('type', 10)
+            ->value('relation_id');
+        $ids = explode(',', $list);
+        if (!$ids) {
+            return error(1000, '还没有推荐');
+        }
+
+        $lists = Lists::select('id', 'title', 'subtitle', 'cover', 'num')
+            ->with(['works' => function ($query) {
+                $query->select('works_id', 'user_id', 'title', 'cover_img')
+                    ->where('status', 4)
+                    ->limit(3)
+                    ->inRandomOrder();
+            }, 'works.user' => function ($query) {
+                $query->select('id', 'nickname', 'headimg');
+            }])->whereIn('id', $ids)
+            ->where('type', $type)
+            ->where('status', 1)
+            ->first();
+        return success($lists);
     }
 
 
