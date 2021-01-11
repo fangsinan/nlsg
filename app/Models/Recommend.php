aa5dde53075ac093fbb54c82cbe41eec2bc8362d
thinkphper <thinkphper@gmail.com>
2021-01-11 18:18:24 +0800

    修改

--- app/Models/Recommend.php
+++ app/Models/Recommend.php
@@ -137,24 +137,12 @@ class Recommend extends Base
 
     public  function getLiveRelation($uid, $list)
     {
-        if (!$uid){
-            return  false;
-        }
-
          $channel = LiveInfo::where('live_pid', $list->id)
                      ->where('status', 1)
                      ->orderBy('id','desc')
                      ->first();
          $isSub = Subscribe::isSubscribe($uid, $list->id, 3);
-//         if (strtotime($channel['begin_at']) > time()) {
-//            $list['live_status'] = '1';
-//         } else {
-//            if (strtotime($channel['end_at']) < time()) {
-//                $list['live_status'] = '2';
-//            } else {
-//                $list['live_status'] = '3';
-//            }
-//         }
+
          if ($channel->is_begin ==0 && $channel->is_finish==0){
              $list['live_status'] = 1;
          } elseif ($channel->is_begin ==1 && $channel->is_finish==0){
