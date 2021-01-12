a29fdc5247861e0462a7164aeed3d4e7070a63e0
thinkphper <thinkphper@gmail.com>
2021-01-12 18:11:45 +0800

    修改直播回放

--- app/Http/Controllers/Api/V4/LiveController.php
+++ app/Http/Controllers/Api/V4/LiveController.php
@@ -147,7 +147,7 @@ class LiveController extends Controller
         $lists = LiveInfo::with('user:id,nickname', 'live:id,title,describe,price,cover_img,begin_at,type,playback_price,is_free,password')
             ->select('id', 'live_pid', 'user_id')
             ->where('status', 1)
-            ->whereNotNull('playback_url')
+            ->where('playback_url','!=','')
             ->orderBy('id')
             ->limit(2)
             ->get()
