078a29ead6553d5b45c1687bc8efe86a62524ff2
thinkphper <thinkphper@gmail.com>
2021-01-12 18:14:07 +0800

    修改

--- app/Http/Controllers/Api/V4/LiveController.php
+++ app/Http/Controllers/Api/V4/LiveController.php
@@ -307,7 +307,7 @@ class LiveController extends Controller
         $lists = LiveInfo::with('user:id,nickname', 'live:id,title,describe,price,cover_img,begin_at,type,playback_price,is_free,password')
             ->select('id', 'live_pid', 'user_id')
             ->where('status', 1)
-            ->whereNotNull('playback_url')
+            ->where('playback_url','!=','')
             ->orderBy('begin_at', 'desc')
             ->paginate(10)
             ->toArray();
