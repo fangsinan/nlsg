f7e4062f189892a5e35f14d53f9cfe3f7ff09f3c
thinkphper <thinkphper@gmail.com>
2021-01-19 17:20:25 +0800

    修改直播

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 
-class Live extends Model
+class Live extends Base
 {
     protected $table = 'nlsg_live';
 
