9cdd688be81d49e298f2397c442d2a1ac558a6a2
fangsinan <15650701817@163.com>
2021-01-22 12:18:02 +0800

    no message

--- app/Http/Controllers/Api/V4/WorksController.php
+++ app/Http/Controllers/Api/V4/WorksController.php
@@ -944,13 +944,15 @@ class WorksController extends Controller
             'end_time' => date("Y-m-d H:i:s", $endtime),
             'relation_id' => $relation_id,
         ];
-        Subscribe::firstOrCreate($subscribe);
-
-        if($sub_type == 1 || $sub_type ==6){
-            Column::where(['id' => $relation_id])->increment('subscribe_num');
-        }else if($sub_type == 2){
-            Works::where(['id' => $relation_id])->increment('subscribe_num', 1);
+        $sub_res = Subscribe::firstOrCreate($subscribe);
+        if($sub_res->wasRecentlyCreated){
+            if($sub_type == 1 || $sub_type ==6){
+                Column::where(['id' => $relation_id])->increment('subscribe_num');
+            }else if($sub_type == 2){
+                Works::where(['id' => $relation_id])->increment('subscribe_num', 1);
+            }
         }
+
 
         return $this->success();
     }
