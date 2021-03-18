ee79e9e8fd45992128a61560d2227d37d98b7952
fangsinan <15650701817@163.com>
2021-03-18 16:13:14 +0800

    no message

--- app/Http/Controllers/Api/V4/LiveController.php
+++ app/Http/Controllers/Api/V4/LiveController.php
@@ -888,6 +888,20 @@ class LiveController extends Controller
         //校验推客id是否有效
         $tweeter_code = $checked['tweeter_code'];
 
+        $from_live_info_id = $input['from_live_info_id'] ?? 0;
+        if( $from_live_info_id > 0 ){   //大于0 时说明在直播间买的
+
+            //查看是否有免费直播间的推荐人
+            $liveCountDown = LiveCountDown::select('live_id', 'user_id', 'new_vip_uid')
+                ->where('live_id', $from_live_info_id)
+                ->where('user_id', $this->user['id'])
+                ->first();
+
+            $tweeter_code = $liveCountDown['new_vip_uid'];
+        }
+
+
+
         $list = Live::select('id', 'title', 'price', 'twitter_money', 'is_free')
             ->where('id', $liveId)
             ->first();
@@ -910,6 +924,7 @@ class LiveController extends Controller
             'live_id' => $liveId,
             'pay_type' => $payType,
             'activity_tag' => $activity_tag,
+            'remark' => $from_live_info_id,
 
         ];
         $order = Order::firstOrCreate($data);
