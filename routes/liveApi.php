a1f0cfb47ae291306e6a8f93af2995461de62498
thinkphper <thinkphper@gmail.com>
2021-03-26 17:39:00 +0800

    修改路由

--- routes/liveApi.php
+++ routes/liveApi.php
@@ -2,7 +2,7 @@
 //后台
 Route::group(['namespace' => 'Live\V4', 'prefix' => 'live_v4'], function () {
 
-    Route::get('index/index', 'IndexController@index');
+    Route::get('index/statistics', 'IndexController@index');
     Route::get('index/lives', 'IndexController@lives');
     Route::get('comment/index', 'CommentController@index');
     Route::get('sub/index', 'SubscribeController@index');
