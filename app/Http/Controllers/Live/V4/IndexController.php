d957f33cf2085f4a473b1c3269e43650d13aee2d
thinkphper <thinkphper@gmail.com>
2021-03-27 11:45:36 +0800

    增加直播封面

--- app/Http/Controllers/Live/V4/IndexController.php
+++ app/Http/Controllers/Live/V4/IndexController.php
@@ -94,7 +94,7 @@ class IndexController extends ControllerBackend
             });
 
         $lists = $query->select('id', 'user_id', 'title', 'price',
-            'order_num', 'status', 'created_at')
+            'order_num', 'status', 'created_at','cover_img')
             ->orderBy('created_at', 'desc')
             ->paginate(10)
             ->toArray();
