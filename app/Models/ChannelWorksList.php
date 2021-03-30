0490c4c2ff5763788598c4c65d20c8f54cec29db
zouxuguang <>
2021-03-30 11:21:26 +0800

    no message

--- app/Models/ChannelWorksList.php
+++ app/Models/ChannelWorksList.php
@@ -255,6 +255,7 @@ class ChannelWorksList extends Base
                 ->where('activity_tag','cytx')->where('pay_price','>','1')
                 ->first();
             $is_sub=0;
+            echo $user_id.'||'.$live_team[0]['id'];
             if(!empty($Order)){
                 $is_sub=1;
             }
