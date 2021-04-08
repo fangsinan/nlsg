28f9e8fa94470cf2be23671859e62d3d1534d9e8
thinkphper <thinkphper@gmail.com>
2021-04-08 17:18:32 +0800

    修改直播详情

--- app/Http/Controllers/Live/V4/IndexController.php
+++ app/Http/Controllers/Live/V4/IndexController.php
@@ -708,7 +708,7 @@ class IndexController extends ControllerBackend
     public function info(Request $request)
     {
         $id = $request->get('id');
-        $live = Live::select('id', 'title', 'cover_img', 'user_id', 'begin_at', 'end_at', 'price', 'twitter_money', 'helper', 'content')
+        $live = Live::select('id', 'title', 'describe', 'cover_img', 'user_id', 'begin_at', 'end_at', 'price', 'twitter_money', 'helper', 'content')
             ->with(['livePoster'])
             ->where('id', $id)->first();
         if (!$live) {
