1a93a506425a700270ef88c153eeff834122a979
fangsinan <15650701817@163.com>
2021-04-09 14:09:26 +0800

    搜索

---
+++ app/Models/LiveUrl.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class LiveUrl extends Model
+{
+    protected $table = 'nlsg_live_url';
+
+    static function search($keywords)
+    {
+        $res = Live::select('id', 'name', 'content', 'url', 'image', 'img', 'user_id')
+            ->where('is_del', 0)
+//            ->where(function ($query) use ($keywords) {
+//                $query->orWhere('name', 'LIKE', "%$keywords%");
+//            })
+            ->get();
+
+        return ['res' => $res, 'count' => $res->count()];
+    }
+}
