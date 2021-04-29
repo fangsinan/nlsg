5f3a3606cde5e92f45fe3aa68e195aba006f4f75
wangxh <>
2021-04-29 15:00:48 +0800

    no message

--- routes/adminApi.php
+++ routes/adminApi.php
@@ -174,23 +174,6 @@ Route::group(['namespace' => 'Admin\V4', 'prefix' => 'admin_v4'], function () {
         Route::get('role/role_list', 'RoleController@roleList');
         Route::get('role/role_select_list', 'RoleController@roleSelectList');
 
-        //直播管理部分
-        Route::get('live_index/statistics', 'IndexController@index');
-        Route::get('live_index/statistics_img_data', 'IndexController@statistics_img_data');
-        Route::get('live_index/data', 'IndexController@data');
-        Route::get('live_index/check_helper', 'IndexController@checkHelper');
-        Route::get('live_index/live_users', 'IndexController@getLiveUsers');
-        Route::get('live_index/lives', 'IndexController@lives');
-        Route::post('live_index/create', 'IndexController@create');
-        Route::post('live_live/delete', 'IndexController@delete');
-        Route::get('live_live/info', 'IndexController@info');
-        Route::get('live_comment/index', 'CommentController@index');
-        Route::get('live_comment/show', 'CommentController@show');
-        Route::post('live_comment/delete', 'CommentController@delete');
-        Route::get('live_sub/index', 'SubscribeController@index');
-        Route::get('live_order/list', 'OrderController@list');
-        Route::get('live_order/inviter_list', 'OrderController@inviterLiveList');
-
     });
 
     Route::post('vip/create_vip', 'VipController@createVip');
--- routes/liveApi.php
+++ routes/liveApi.php
@@ -4,21 +4,21 @@ Route::group(['namespace' => 'Live\V4', 'prefix' => 'live_v4'], function () {
 
 //    Route::group(['middleware' => ['auth.jwt']], function () {
     Route::group(['middleware' => ['auth.backend.jwt']], function () {
-//        Route::get('index/statistics', 'IndexController@index');
-//        Route::get('index/statistics_img_data', 'IndexController@statistics_img_data');
-//        Route::get('index/data', 'IndexController@data');
-//        Route::get('index/check_helper', 'IndexController@checkHelper');
-//        Route::get('index/live_users', 'IndexController@getLiveUsers');
-//        Route::get('index/lives', 'IndexController@lives');
-//        Route::post('index/create', 'IndexController@create');
-//        Route::post('live/delete', 'IndexController@delete');
-//        Route::get('live/info', 'IndexController@info');
-//        Route::get('comment/index', 'CommentController@index');
-//        Route::get('comment/show', 'CommentController@show');
-//        Route::post('comment/delete', 'CommentController@delete');
-//        Route::get('sub/index', 'SubscribeController@index');
-//        Route::get('order/list', 'OrderController@list');
-//        Route::get('order/inviter_list', 'OrderController@inviterLiveList');
+        Route::get('live_index/statistics', 'IndexController@index');
+        Route::get('live_index/statistics_img_data', 'IndexController@statistics_img_data');
+        Route::get('live_index/data', 'IndexController@data');
+        Route::get('live_index/check_helper', 'IndexController@checkHelper');
+        Route::get('live_index/live_users', 'IndexController@getLiveUsers');
+        Route::get('live_index/lives', 'IndexController@lives');
+        Route::post('live_index/create', 'IndexController@create');
+        Route::post('live_live/delete', 'IndexController@delete');
+        Route::get('live_live/info', 'IndexController@info');
+        Route::get('live_comment/index', 'CommentController@index');
+        Route::get('live_comment/show', 'CommentController@show');
+        Route::post('live_comment/delete', 'CommentController@delete');
+        Route::get('live_sub/index', 'SubscribeController@index');
+        Route::get('live_order/list', 'OrderController@list');
+        Route::get('live_order/inviter_list', 'OrderController@inviterLiveList');
     });
 
 });
