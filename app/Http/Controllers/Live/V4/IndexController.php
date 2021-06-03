789f177934fda16f40ad3c3abddeda57f1f218f0
fangsinan <15650701817@163.com>
2021-06-03 14:07:11 +0800

    no message

--- app/Http/Controllers/Live/V4/IndexController.php
+++ app/Http/Controllers/Live/V4/IndexController.php
@@ -479,12 +479,12 @@ class IndexController extends ControllerBackend
 
         if (!empty($input['id'])) {
             Live::where('id', $input['id'])->update($data);
-            $live_info_data = LiveInfo::where('live_pid', '=', $input['id'])->first('id','task_id','begin_at','end_at','push_end_time');
-            $live_info_id = $live_info_data['id'];
+            $info_first = LiveInfo::where('live_pid', '=', $input['id'])->first('id','task_id','begin_at','end_at','push_end_time');
+            $live_info_id = $info_first['id'];
              // 删除拉流任务
-            if($live_info_data['task_id'] ){
+            if($info_first['task_id'] ){
                 //当开始时间、结束时间、推流结束时间 变了修改     否则  不变
-                if(($live_info_data['begin_at'] != $begin_at) || ($live_info_data['end_at'] != $end_at) || ($live_info_data['push_end_time'] != $temp_push_end_time) ){
+                if(($info_first['begin_at'] != $begin_at) || ($info_first['end_at'] != $end_at) || ($info_first['push_end_time'] != $temp_push_end_time) ){
                     //校验 推拉流
                     LiveInfo::liveUrlEdit('del',$live_info_id);
                 }
