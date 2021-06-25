21038e64b0018d60ebd4157381b190dc8e5d1c4a
thinkphper <thinkphper@gmail.com>
2021-06-25 14:06:36 +0800

    修改消息通知

--- app/Http/Controllers/Admin/V4/TaskController.php
+++ app/Http/Controllers/Admin/V4/TaskController.php
@@ -68,6 +68,10 @@ class TaskController extends ControllerBackend
     {
         $input = $request->all();
         if ($input['send_type'] ==1){
+            $user = User::where('id', $input['user_id'])->first();
+            if (!$user){
+                return error('没有找到用户');
+            }
             Task::send($input['type'], $input['user_id'], $input['id'],0, $input['title']);
         } elseif($input['send_type'] ==2){
             JPush::pushNow('all', $input['title'], ['type'=>$input['type'], 'id'=>$input['id']]);
