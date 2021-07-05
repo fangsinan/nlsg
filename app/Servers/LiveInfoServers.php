edf400f54a70aa90e7455e657aa3d4c54a508308
wangxh <>
2021-07-05 11:19:18 +0800

    no message

--- app/Servers/LiveInfoServers.php
+++ app/Servers/LiveInfoServers.php
@@ -4,6 +4,8 @@
 namespace App\Servers;
 
 
+use App\Models\BackendLiveRole;
+use App\Models\Live;
 use Illuminate\Support\Facades\DB;
 
 class LiveInfoServers
@@ -23,6 +25,7 @@ class LiveInfoServers
                 $query->on('cd.user_id', '=', 's.user_id')->where('cd.live_id', '=', $live_id);
             })
             ->leftJoin('nlsg_user as tu', 'tu.id', '=', 'cd.new_vip_uid')
+            ->leftJoin('nlsg_backend_live_role as lr', 'tu.id', '=', 'lr.son_id')
             ->where('s.type', '=', 3)
             ->where('s.relation_id', '=', $live_id);
 
@@ -31,29 +34,33 @@ class LiveInfoServers
         //用户id 推荐人id
         //用户手机 推荐人手机
         if (!empty($params['nickname'] ?? '')) {
-            $query->where('u.nickname','like','%'.$params['nickname'].'%');
+            $query->where('u.nickname', 'like', '%' . $params['nickname'] . '%');
         }
         if (!empty($params['id'] ?? '')) {
-            $query->where('u.id','=',$params['id']);
+            $query->where('u.id', '=', $params['id']);
         }
         if (!empty($params['phone'] ?? '')) {
-            $query->where('u.phone','like','%'.$params['phone'].'%');
+            $query->where('u.phone', 'like', '%' . $params['phone'] . '%');
         }
 
         if (!empty($params['t_nickname'] ?? '')) {
-            $query->where('tu.nickname','like','%'.$params['t_nickname'].'%');
+            $query->where('tu.nickname', 'like', '%' . $params['t_nickname'] . '%');
         }
         if (!empty($params['t_user_id'] ?? '')) {
-            $query->where('tu.user_id','=',$params['tu_user_id']);
+            $query->where('tu.user_id', '=', $params['tu_user_id']);
         }
         if (!empty($params['t_phone'] ?? '')) {
-            $query->where('tu.phone','like','%'.$params['t_phone'].'%');
+            $query->where('tu.phone', 'like', '%' . $params['t_phone'] . '%');
+        }
+
+        if (!empty($params['son_flag'] ?? '')) {
+            $query->where('son_flag', 'like', '%' . $params['son_flag'] . '%');
         }
 
         $query->orderBy('s.created_at', 'desc')
             ->select([
                 's.id', 's.user_id', 'u.phone', 'u.nickname', 'tu.id as t_user_id', 'tu.phone as t_phone',
-                'tu.nickname as t_nickname', 's.created_at', 's.relation_id'
+                'tu.nickname as t_nickname', 'lr.son_flag', 's.created_at', 's.relation_id'
             ]);
 
         return $query->paginate($size);
@@ -61,6 +68,19 @@ class LiveInfoServers
 
     public function liveOrder($params)
     {
+        $size = $params['size'] ?? 10;
+
+        $live_id = $params['live_id'] ?? 0;
+        if (empty($live_id)) {
+            return ['code' => false, 'msg' => 'live_id错误'];
+        }
+        $check_live_id = Live::where('id', '=', $live_id)->first();
+        if (empty($check_live_id)) {
+            return ['code' => false, 'msg' => 'live_id错误'];
+        }
+
+        $twitter_id_list = $this->twitterIdList('', $check_live_id->user_id);
+        return $twitter_id_list;
 
     }
 
@@ -77,6 +97,15 @@ class LiveInfoServers
     public function onlineNum($params)
     {
 
+    }
+
+    //获得名下所有渠道的user_id
+    public function twitterIdList($phone = '', $user_id = '')
+    {
+        return BackendLiveRole::where(function ($query) use ($phone, $user_id) {
+            $query->where('parent', '=', $phone)
+                ->orWhere('parent_id', '=', $user_id);
+        })->pluck('son_id')->toArray();
     }
 
 }
