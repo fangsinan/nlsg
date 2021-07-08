f4c11f01562f3dc82369c5dd8888a85d86e759b9
fangsinan <15650701817@163.com>
2021-07-08 10:32:29 +0800

    no message

--- app/Models/ImMsg.php
+++ app/Models/ImMsg.php
@@ -28,4 +28,62 @@ class ImMsg extends Base
         }
     }
 
+
+    public static function MsgBody($msg_type, $params){
+
+        $msg_content_add = [];
+        switch ($msg_type){
+            case 'TIMTextElem' :  //文本消息元素
+                $msg_content_add['text']            = $params['Text'];
+                break;
+            case 'TIMFaceElem' : //表情消息元素
+                $msg_content_add['index']           = $params['Index'];
+                $msg_content_add['data']            = $params['Data'];
+                break;
+            case 'TIMSoundElem' ://语音消息元素
+                $msg_content_add['Url']             = $params['Url'];
+                $msg_content_add['Size']            = $params['Size'];
+                $msg_content_add['Second']          = $params['Second'];
+                $msg_content_add['Download_Flag']   = 2;
+                break;
+            case 'TIMImageElem' ://图片元素
+                $msg_content_add['Type']        = $params['Type'];
+                $msg_content_add['Size']        = $params['Size'];
+                $msg_content_add['Width']       = $params['Width'];
+                $msg_content_add['Height']      = $params['Height'];
+                $msg_content_add['URL']         = $params['URL'];
+                $msg_content_add['UUID']        = $params['UUID'];
+                $msg_content_add['ImageFormat'] = $params['ImageFormat'];
+
+                break;
+            case 'TIMFileElem' ://文件类型元素
+                $msg_content_add['Url']             = $params['Url'];
+                $msg_content_add['FileSize']        = $params['FileSize'];
+                $msg_content_add['FileName']        = $params['FileName'];
+                $msg_content_add['Download_Flag']   = $params['Download_Flag'];
+                break;
+
+            case 'TIMVideoFileElem' : //视频类型元素
+                $msg_content_add['VideoUrl']            = $params['VideoUrl'];
+                $msg_content_add['VideoSize']           = $params['VideoSize'];
+                $msg_content_add['VideoSecond']         = $params['VideoSecond'];
+                $msg_content_add['VideoFormat']         = $params['VideoFormat'];
+                $msg_content_add['VideoDownloadFlag']   = $params['VideoDownloadFlag'];
+//                $msg_content_add['ThumbUrl']            = $params['ThumbUrl'];
+//                $msg_content_add['ThumbSize']           = $params['ThumbSize'];
+//                $msg_content_add['ThumbWidth']          = $params['ThumbWidth'];
+//                $msg_content_add['ThumbHeight']         = $params['ThumbHeight'];
+//                $msg_content_add['ThumbFormat']         = $params['ThumbFormat'];
+                break;
+
+            default :
+                return [];
+                break;
+        }
+
+
+        return [ 'MsgType' => $msg_type,  'MsgContent' => $msg_content_add, ];
+    }
+
+
 }
