5cec21ec087d54f9fd8ee8b730987d72f9df4d3c
fangsinan <15650701817@163.com>
2021-07-13 17:50:46 +0800

    no message

--- app/Http/Controllers/Api/V4/ImController.php
+++ app/Http/Controllers/Api/V4/ImController.php
@@ -9,25 +9,20 @@ use Libraries\ImClient;
 class ImController extends Controller
 {
     /**
-    * @api {get} api/v4/im/get_user_sig  用户签名
-    * @apiVersion 4.0.0
-    * @apiName  get_user_sig
-    * @apiGroup im
-    * @apiParam {int} user_id  user_id  数组类型
-
-    * @apiSuccess {string} data 内容
-    *
-    * @apiSuccessExample  Success-Response:
-    * HTTP/1.1 200 OK
-    * {
-    *      "code": 200,
-    *      "msg" : '成功',
-    *      "data": {
-    *          
-    *       }
-    * }
-    *
-    */
+     * @api {get} api/v4/im/get_user_sig  用户签名
+     * @apiVersion 4.0.0
+     * @apiName  get_user_sig
+     * @apiGroup im
+     * @apiParam {int} user_id  user_id  数组类型
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+    {
+    "code": 200,
+    "msg": "成功",
+    "data": {}
+    }
+     */
     public  function  getUserSig(Request $request)
     {
         $user_id = $request->get('user_id');
