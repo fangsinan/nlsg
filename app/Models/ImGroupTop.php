c98b18781152d74d863f7c13755726ada077432f
wangxh <>
2021-07-27 11:44:50 +0800

    no message

--- app/Models/ImGroupTop.php
+++ app/Models/ImGroupTop.php
@@ -7,4 +7,7 @@ namespace App\Models;
 class ImGroupTop extends Base
 {
     protected $table = 'nlsg_im_group_top';
+    protected $fillable = [
+        'id','user_id','group_id','created_at','updated_at'
+    ];
 }
