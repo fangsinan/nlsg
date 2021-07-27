7729a03f2262e0ef4ca98323e7d3e077faa5903f
wangxh <>
2021-07-27 11:53:41 +0800

    no message

--- app/Servers/ImGroupServers.php
+++ app/Servers/ImGroupServers.php
@@ -24,7 +24,8 @@ class ImGroupServers
                 'g.id', 'g.group_id', 'g.operator_account', 'g.owner_account', 'g.type', 'g.name',
                 'g.status', 'g.created_at', 'owner.phone as owner_phone', 'owner.id as owner_id',
                 'owner.nickname as owner_nickname', 'g.member_num', 'works_id',
-                DB::raw('(case gt.id when gt.id > 0 then 1 else 0 end) as is_top'),
+//                DB::raw('(case gt.id when gt.id > 0 then 1 else 0 end) as is_top'),
+                DB::raw('if(gt.id<1,0,1) AS is_top'),
                 DB::raw('2000 as max_num')
             ])->orderBy('gt.id', 'desc');
 
@@ -46,7 +47,6 @@ class ImGroupServers
         }
 
         $query->orderBy('g.id', 'desc');
-
 
         $list = $query->paginate($size);
 
