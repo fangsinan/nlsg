3eb4c351fc859985cd5cc342848d2298919c0753
zouxuguang <>
2021-07-28 09:41:20 +0800

    no message

---
+++ vendor/alibabacloud/sdk/src/Hitsdb/V20200615/HitsdbApiResolver.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace AlibabaCloud\Hitsdb\V20200615;
+
+use AlibabaCloud\Client\Resolver\ApiResolver;
+
+/**
+ * @method DescribeRegions describeRegions(array $options = [])
+ * @method GetLindormInstance getLindormInstance(array $options = [])
+ * @method GetLindormInstanceList getLindormInstanceList(array $options = [])
+ */
+class HitsdbApiResolver extends ApiResolver
+{
+}
+
+class Rpc extends \AlibabaCloud\Client\Resolver\Rpc
+{
+    /** @var string */
+    public $product = 'hitsdb';
+
+    /** @var string */
+    public $version = '2020-06-15';
+
+    /** @var string */
+    public $method = 'POST';
+
+    /** @var string */
+    public $serviceCode = 'hitsdb';
+}
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getSecurityToken()
+ * @method $this withSecurityToken($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getOwnerAccount()
+ * @method $this withOwnerAccount($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getAcceptLanguage()
+ * @method $this withAcceptLanguage($value)
+ */
+class DescribeRegions extends Rpc
+{
+}
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getSecurityToken()
+ * @method $this withSecurityToken($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getOwnerAccount()
+ * @method $this withOwnerAccount($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getInstanceId()
+ * @method $this withInstanceId($value)
+ */
+class GetLindormInstance extends Rpc
+{
+}
+
+/**
+ * @method string getResourceOwnerId()
+ * @method $this withResourceOwnerId($value)
+ * @method string getSupportEngine()
+ * @method $this withSupportEngine($value)
+ * @method string getPageNumber()
+ * @method $this withPageNumber($value)
+ * @method string getSecurityToken()
+ * @method $this withSecurityToken($value)
+ * @method string getPageSize()
+ * @method $this withPageSize($value)
+ * @method string getQueryStr()
+ * @method $this withQueryStr($value)
+ * @method string getResourceOwnerAccount()
+ * @method $this withResourceOwnerAccount($value)
+ * @method string getOwnerAccount()
+ * @method $this withOwnerAccount($value)
+ * @method string getOwnerId()
+ * @method $this withOwnerId($value)
+ * @method string getServiceType()
+ * @method $this withServiceType($value)
+ */
+class GetLindormInstanceList extends Rpc
+{
+}
