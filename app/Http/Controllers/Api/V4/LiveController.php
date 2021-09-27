e3c1a75102a1765500730264dd5bfa9c0502b2b2
fangsinan <15650701817@163.com>
2021-09-27 16:38:47 +0800

    no message

--- app/Http/Controllers/Api/V4/LiveController.php
+++ app/Http/Controllers/Api/V4/LiveController.php
@@ -981,7 +981,7 @@ class LiveController extends Controller
                 'push_type'=>9,
             ])->orderBy('created_at', 'desc')->value('push_gid');
             if(!empty($push_gid)){
-                $push_live = Live::select("*")->where(['id'=>$push_gid])->first();
+                $push_live = Live::select("id","title", "price","type","cover_img")->where(['id'=>$push_gid])->first();
             }
         }
         $data = [
