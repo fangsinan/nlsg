040acf9364cb52672863b6e91ef84debbc30c1a4
wangxh <>
2021-11-10 10:19:30 +0800

    直播创建 修改

--- app/Models/BackendLiveDataRole.php
+++ app/Models/BackendLiveDataRole.php
@@ -7,5 +7,7 @@ class BackendLiveDataRole extends Base
 {
     protected $table = 'nlsg_backend_live_data_role';
 
-
+    protected $fillable = [
+        'user_id', 'live_id', 'created_at', 'updated_at'
+    ];
 }
