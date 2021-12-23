c7a3f305d351d25bf2f31c89b29214a8ee9d2e05
fangsinan <15650701817@163.com>
2021-12-23 10:56:02 +0800

    no message

--- app/Http/Controllers/Api/V4/LikeController.php
+++ app/Http/Controllers/Api/V4/LikeController.php
@@ -18,7 +18,8 @@ class LikeController extends Controller
      * @api {post} api/v4/like 点赞
      * @apiVersion 4.0.0
      * @apiParam  id  评论id
-     * @apiParam  type  类型 1.想法 2.百科
+     * @apiParam  type  类型 1.想法 2.百科 3短视频
+     * @apiParam  comment_type  类型 1.主评论 2 次级评论
      * @apiGroup Api
      *
      * @apiSuccess {String} token   token
@@ -37,35 +38,37 @@ class LikeController extends Controller
     {
         $id   = $request->input('id');
         $type = $request->input('type');
-        if (!$id || !$type){
+        $comment_type = $request->input('comment_type');
+        if (!$id || !$type || $comment_type){
             return error(1000, '参数不全');
         }
-        $list = Like::where(['relation_id'=> $id, 'user_id'=> $this->user['id'], 'type'=>$type])->first();
+        $list = Like::where(['comment_type'=>$comment_type, 'relation_id'=> $id, 'user_id'=> $this->user['id'], 'type'=>$type])->first();
         if ($list){
             return error(1000,'不要重复操作');
         }
 
         $res = Like::create([
+            'comment_type'=>$comment_type,
             'relation_id' => $id,
             'user_id'     => $this->user['id'],
             'type'        => $type
         ]);
         if ($res){
-            if ($type ==1){
-                $comment = Comment::where('id', $id)->first();
-                $notify = new Notify();
-                $notify->from_uid = $this->user['id'];
-                $notify->to_uid   = $comment->user_id;
-                $notify->source_id= $id;
-                $notify->type     = 1;
-                $notify->subject  = '喜欢了你的想法';
-                $notify->save();
-            }
+//            if ($type ==1){
+//                $comment = Comment::where('id', $id)->first();
+//                $notify = new Notify();
+//                $notify->from_uid = $this->user['id'];
+//                $notify->to_uid   = $comment->user_id;
+//                $notify->source_id= $id;
+//                $notify->type     = 1;
+//                $notify->subject  = '喜欢了你的想法';
+//                $notify->save();
+//            }
             //增加喜欢
             Comment::where('id', $id)->increment('like_num');
-            $from_user = User::where('id', $this->user['id'])->value('nickname');
+//            $from_user = User::where('id', $this->user['id'])->value('nickname');
             //发送通知
-            Task::send(13, $comment->user_id, $id, 0, '',false,false, 0, $from_user, $comment->type, $comment->relation_id);
+//            Task::send(13, $comment->user_id, $id, 0, '',false,false, 0, $from_user, $comment->type, $comment->relation_id);
 
             return success('操作成功');
         }
