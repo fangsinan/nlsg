6149653c45fc803b585a48edb3b7455cf5953742
wangxh <>
2022-01-21 19:30:43 +0800

    no message

--- app/Servers/V5/CampServers.php
+++ app/Servers/V5/CampServers.php
@@ -115,20 +115,6 @@ where sub.relation_id = $works_id and sub.type = 7 and sub.`status` = 1 and sub.
         $total = DB::select($count_sql);
         $data  = DB::select($query_sql);
 
-
-//current_page: 1
-//data: [{id: 12808957, user_id: 7221831, is_end: 1, end_time: "2022-01-18 21:57:36",…},…]
-//first_page_url: "http://app.v4.api.nlsgapp.com/api/admin_v4/class/camp_clock_in_info?page=1"
-//from: 1
-//last_page: 30
-//last_page_url: "http://app.v4.api.nlsgapp.com/api/admin_v4/class/camp_clock_in_info?page=30"
-//next_page_url: "http://app.v4.api.nlsgapp.com/api/admin_v4/class/camp_clock_in_info?page=2"
-//path: "http://app.v4.api.nlsgapp.com/api/admin_v4/class/camp_clock_in_info"
-//per_page: "10"
-//prev_page_url: null
-//to: 10
-//total: 298
-
         foreach ($data as &$v) {
             $v->user_info = [
                 'id'       => $v->user_id ?? '',
@@ -142,105 +128,61 @@ where sub.relation_id = $works_id and sub.type = 7 and sub.`status` = 1 and sub.
         $res['total']        = $total[0]->counts ?? 0;
 
         return $res;
-
-
-//        $query = History::query()
-//            ->where('relation_type', '=', 5)
-//            ->where('relation_id', '=', $works_id)
-//            ->where('info_id', '=', $works_info_id)
-//            ->select([
-//                'id', 'user_id', 'is_end',
-//                DB::raw("IF(is_end = 0,'-',(IF(end_time is NULL,updated_at,end_time))) as end_time")
-//            ])
-//            ->with(['userInfo:id,phone,nickname']);
-//
-//        if ($is_end !== -1) {
-//            $query->where('is_end', '=', $is_end);
-//        }
-//
-//        return $query->paginate($params['size'] ?? 10);
-
-//        $query = Subscribe::query()->from('nlsg_subscribe as sub')
-//            ->leftJoin('nlsg_history as his', function ($q) use ($works_id, $works_info_id) {
-//                $q->on('sub.user_id', '=', 'his.user_id')
-//                    ->where('his.relation_type', '=', 5)
-//                    ->where('his.relation_id', '=', $works_id)
-//                    ->where('his.info_id', '=', $works_info_id);
-//            })
-//            ->where('sub.relation_id', '=', $works_id)
-//            ->where('sub.type', '=', 7)
-//            ->where('sub.status', '=', 1)
-//            ->where('sub.is_del', '=', 0)
-//            ->select([
-//                'sub.id', 'sub.user_id',
-//                DB::raw("if(his.is_end = 1,1,0) as is_end"),
-//                DB::raw("if(his.is_end = 1,(IF(his.end_time is NULL,his.updated_at,his.end_time)),'-') as end_time"),
-//            ])
-//            ->with(['userInfo:id,phone,nickname']);
-//
-//        if ($is_end === 1) {
-//            $query->where('his.is_end', '=', 1);
-//        }
-//
-//        if ($is_end === 0){
-////            $query->whereRaw('his.is_end =0 or his.is_end is NULL');
-//        }
-//
-//        return $query->paginate($params['size'] ?? 10);
-
-//        $query = Subscribe::query()
-//            ->with([
-//                'userInfo:id,phone,nickname',
-//                'historyInfo' => function ($q) use ($works_id, $works_info_id) {
-//                    $q->where('relation_type', '=', 5)
-//                        ->where('relation_id', '=', $works_id)
-//                        ->where('info_id', '=', $works_info_id)
-//                        ->select(['id', 'user_id', 'is_end', 'updated_at', 'end_time']);
-//                }
-//            ])
-//            ->where('relation_id', '=', $works_id)
-//            ->where('type', '=', 7)
-//            ->where('status', '=', 1)
-//            ->where('is_del', '=', 0)
-//            ->select(['id', 'user_id']);
-//
-//
-//        if ($is_end === 1) {
-//            $query->whereHas('historyInfo', function ($q) use ($works_id, $works_info_id) {
-//                $q->where('relation_type', '=', 5)
-//                    ->where('relation_id', '=', $works_id)
-//                    ->where('info_id', '=', $works_info_id)
-//                    ->where('is_end', '=', 1);
-//            });
-//        }
-
-//        if ($is_end === 0){
-//        $query->whereHas('historyInfo', function ($q) use ($works_id, $works_info_id) {
-//            $q->where('relation_type', '=', 5)
-//                ->where('relation_id', '=', $works_id)
-//                ->where('info_id', '=', $works_info_id)
-//                ->where('is_end', '!=', 1);
-//        });
-
-//            $query->where(function($q)use($works_id,$works_info_id){
-//                $q->doesntExist('historyInfo');
-//                    ->orWhereHas('historyInfo',function($q)use($works_id,$works_info_id){
-//                        $q->where('relation_type','=',5)
-//                            ->where('relation_id','=',$works_id)
-//                            ->where('info_id','=',$works_info_id)
-//                            ->where('is_end','!=',1);
-//                    });
-//            });
-//        }
-
-//        DB::connection()->enableQueryLog();
-//        $query->limit(10)->get();
-//        dd(DB::getQueryLog());
-
-//        return $query->paginate($params['size'] ?? 10);
-
-
     }
 
+    public function CampClockInInfo_2($params) {
+        $works_id      = $params['works_id'] ?? 0;
+        $works_info_id = $params['works_info_id'] ?? 0;
+        $is_end        = (int)($params['is_end'] ?? -1);
+
+        if (empty($works_id) || empty($works_info_id)) {
+            return ['code' => false, 'msg' => '参数错误'];
+        }
+
+        $query = Subscribe::query()
+            ->with([
+                'userInfo:id,phone,nickname',
+                'historyInfo' => function ($q) use ($works_id, $works_info_id) {
+                    $q->where('relation_type', '=', 5)
+                        ->where('relation_id', '=', $works_id)
+                        ->where('info_id', '=', $works_info_id)
+                        ->select(['id', 'user_id', 'is_end', 'updated_at', 'end_time']);
+                }
+            ])
+            ->where('relation_id', '=', $works_id)
+            ->where('type', '=', 7)
+            ->where('status', '=', 1)
+            ->where('is_del', '=', 0)
+            ->select(['id', 'user_id']);
+
+
+        if ($is_end === 1) {
+            $query->whereHas('historyInfo', function ($q) use ($works_id, $works_info_id) {
+                $q->where('relation_type', '=', 5)
+                    ->where('relation_id', '=', $works_id)
+                    ->where('info_id', '=', $works_info_id)
+                    ->where('is_end', '=', 1);
+            });
+        }
+
+        if ($is_end === 0) {
+            $query->where(function ($q) use ($works_id, $works_info_id) {
+                $q->whereDoesntHave('historyInfo', function ($q) use ($works_id, $works_info_id) {
+                    $q->where('relation_type', '=', 5)
+                        ->where('relation_id', '=', $works_id)
+                        ->where('info_id', '=', $works_info_id)
+                        ->where('is_end', '=', 1);
+                })->orWhereHas('historyInfo', function ($q) use ($works_id, $works_info_id) {
+                    $q->where('relation_type', '=', 5)
+                        ->where('relation_id', '=', $works_id)
+                        ->where('info_id', '=', $works_info_id)
+                        ->where('is_end', '!=', 1);
+                });
+            });
+        }
+
+        return $query->paginate($params['size'] ?? 10);
+
+    }
 
 }
