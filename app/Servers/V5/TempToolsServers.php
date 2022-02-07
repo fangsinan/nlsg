7e23d8405fa5c068f36d7ac4643affd67184a75a
wangxh <>
2022-02-07 17:40:45 +0800

    no message

--- app/Servers/V5/TempToolsServers.php
+++ app/Servers/V5/TempToolsServers.php
@@ -14,7 +14,7 @@ class TempToolsServers
         $size       = 1000;
         $page       = 1;
 
-        if (0) {
+        if (1) {
             //确定查询用户的直播数据
             while ($while_flag) {
                 $temp_query = Order::query()
@@ -22,8 +22,8 @@ class TempToolsServers
                     ->where('pay_price', '=', 49.9)
                     ->where('is_shill', '=', 0)
                     ->where('status', '=', 1)
-                    ->where('live_id','>',177)
-                    ->where('live_id','<=',300)
+                    ->where('live_id', '>', 177)
+                    ->where('live_id', '<=', 300)
                     ->select(['user_id', 'live_id'])
                     ->limit($size)
                     ->offset(($page - 1) * $size)
@@ -37,7 +37,8 @@ class TempToolsServers
                     $temp_live_array = [];
 
                     foreach ($temp_query as $v) {
-                        $temp_user_array[] = ['user_id' => $v['user_id']];
+                        $temp_user_array[] = ['user_id' => $v['user_id'], 'day_flag' => 1];
+                        $temp_user_array[] = ['user_id' => $v['user_id'], 'day_flag' => 2];
                         $temp_live_array[] = ['live_id' => $v['live_id'], 'day_flag' => 1];
                         $temp_live_array[] = ['live_id' => $v['live_id'], 'day_flag' => 2];
                     }
@@ -51,7 +52,7 @@ class TempToolsServers
         }
 
 
-        if (0) {
+        if (1) {
             //匹配直播的首日日期和星期
             $live_check = Live::query()
                 ->from('nlsg_live as l')
@@ -78,7 +79,7 @@ class TempToolsServers
         }
 
 
-        if (0) {
+        if (1) {
             //匹配观看记录
             $online_user_model = [
                 'nlsg_live_online_user',
@@ -87,6 +88,7 @@ class TempToolsServers
                 'nlsg_live_online_user20211211',
                 'nlsg_live_online_user20211223',
                 'nlsg_live_online_user20220105',
+                'nlsg_live_online_user20220207',
             ];
 
             $live_id_array = DB::table('temp_tool_meikan_live')
@@ -98,8 +100,19 @@ class TempToolsServers
                     $sql = 'update temp_tool_meikan_user as mu
 join ' . $uv . ' as ou on mu.user_id = ou.user_id
 set mu.meikan = 1
-where mu.meikan = 0 and ou.live_id = ' . $lv->live_id . ' and ou.online_time_str like "' . $lv->first_day . '%"';
+where mu.meikan = 0 and mu.day_flag = 1 and ou.live_id = ' .
+                        $lv->live_id . ' and ou.online_time_str like "' . $lv->first_day . '%"';
                     DB::select($sql);
+
+                    if ($lv->day_flag === 1){
+                        $sql = 'update temp_tool_meikan_user as mu
+join ' . $uv . ' as ou on mu.user_id = ou.user_id
+set mu.meikan = 1
+where mu.meikan = 0 and mu.day_flag = 2 and ou.live_id = ' .
+                            $lv->live_id . ' and ou.online_time_str like "' . $lv->first_day . '%"';
+                        DB::select($sql);
+                    }
+
                 }
             }
         }
