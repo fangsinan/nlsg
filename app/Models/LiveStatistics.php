53f2e23cd97dbfd52adcd0040ad3bed3cc22a0f7
wangxh <>
2022-03-08 15:56:15 +0800

    直播列表 订阅人数

--- app/Models/LiveStatistics.php
+++ app/Models/LiveStatistics.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Servers\LiveInfoServers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
@@ -14,10 +15,18 @@ class LiveStatistics extends Model
     ];
 
     public static function getCounts($live_id, $type, $channel_user_id) {
+        $lis = new LiveInfoServers();
+        $channel_user_id_list = $lis->twitterIdList('',$channel_user_id);
+        if (empty($channel_user_id_list)){
+            return 0;
+        }
+        $channel_user_id_list[] = $channel_user_id;
+
         return max(doubleval(self::query()
             ->where('live_id', '=', $live_id)
             ->where('type', '=', $type)
-            ->where('channel_user_id', '=', $channel_user_id)
+            ->whereIn('channel_user_id',$channel_user_id_list)
+//            ->where('channel_user_id', '=', $channel_user_id)
             ->value('counts')), 0);
     }
 
