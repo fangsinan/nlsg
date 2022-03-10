ff2578946abac2c2f176c6f0f80ad895c3e65545
fangsinan <15650701817@163.com>
2022-03-10 14:43:27 +0800

    no message

--- app/Http/Controllers/Api/V4/IndexController.php
+++ app/Http/Controllers/Api/V4/IndexController.php
@@ -1287,7 +1287,7 @@ class IndexController extends Controller
 
     public function share(Request $request)
     {
-        dd($_SERVER['HTTP_HOST']);
+        dd($_SERVER['SERVER_ADDR']);
         $input = $request->all();
 
         if( !empty($input['tag']) && $input['tag'] == 1 ){  //训练营分享参数
