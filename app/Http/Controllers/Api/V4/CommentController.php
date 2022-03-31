046dc4556b220d0dc0cec77b56c36f8c4dab8a67
fangsinan <15650701817@163.com>
2022-03-31 14:44:22 +0800

    no message

--- app/Http/Controllers/Api/V4/CommentController.php
+++ app/Http/Controllers/Api/V4/CommentController.php
@@ -468,7 +468,7 @@ class CommentController extends Controller
         if (!$comment){
             return error(1000,'评论不存在');
         }
-        if($comment->user_id !== $this->user['id'] || $this->user['is_community_admin']==0 ){
+        if($comment->user_id !== $this->user['id'] && $this->user['is_community_admin']==0 ){
             return error(1000,'没有权限删除');
         }
 
