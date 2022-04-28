aa834317dc1e11061f6143a07a1e39066b6a38fa
wangxh <>
2022-04-28 13:55:37 +0800

    直播相关定时任务调用方式修改

---
+++ app/Console/Commands/CrontabGiftRedis.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Servers\LiveConsoleServers;
+use Illuminate\Console\Command;
+
+class CrontabGiftRedis extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:CrontabGiftRedis';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '直播打赏入库';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        LiveConsoleServers::CrontabGiftRedis();//直播打赏入库
+    }
+}
