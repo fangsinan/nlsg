a3d43ce1cc8aba9ce6c3b17e1633b2b0db302f2e
wangxh <>
2022-05-05 15:28:26 +0800

    no message

--- app/Http/Controllers/Api/V4/PayController.php
+++ app/Http/Controllers/Api/V4/PayController.php
@@ -243,7 +243,7 @@ class PayController extends Controller
 
         $userInfo = User::find($OrderInfo['user_id']);
         if ($userInfo['is_test_pay'] == 1) {
-            if (empty($userInfo['test_pay_price'])) {
+            if (empty((float)$userInfo['test_pay_price'])) {
                 $OrderInfo['price'] = 0.01;
             }else{
                 $OrderInfo['price'] = $userInfo['test_pay_price'];
