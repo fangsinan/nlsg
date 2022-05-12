bbdb62d56f066d95622666206f075ae963ec342d
wangxh <>
2022-05-12 10:07:37 +0800

    no message

--- app/Servers/V5/DouDianServers.php
+++ app/Servers/V5/DouDianServers.php
@@ -61,13 +61,13 @@ class DouDianServers
     //解密任务 1分一次
     public function decryptJob() {
         //收件人手机解密
-        $this->toDecrypt();
+//        $this->toDecrypt();
 
         //收件人姓名解密
-        $this->toDecrypt(1);
+//        $this->toDecrypt(1);
 
         //收件人详细地址解密
-        $this->toDecrypt(2);
+//        $this->toDecrypt(2);
     }
 
     //拉订单任务 5分钟一次
