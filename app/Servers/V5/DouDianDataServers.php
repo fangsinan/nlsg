f7bcdafe2c1fbd62c40cb4247c9378c62b2a9ba3
wangxh <>
2022-05-19 09:43:11 +0800

    no message

--- app/Servers/V5/DouDianDataServers.php
+++ app/Servers/V5/DouDianDataServers.php
@@ -98,7 +98,7 @@ class DouDianDataServers
             $q->where('create_time', '>=', $create_time_date_begin);
         });
         $query->when($create_time_date_end, function ($q, $create_time_date_end) {
-            $create_time_date_end = strtotime($create_time_date_end) + 59;
+            $create_time_date_end = strtotime($create_time_date_end) + 86400;
             $q->where('create_time', '<=', $create_time_date_end);
         });
 
