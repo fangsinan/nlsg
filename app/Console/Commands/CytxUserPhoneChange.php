0804c766471bf8f254a296a82accb7e146ce1ce6
w <han313500313@126.com>
2022-05-31 17:09:23 +0800

    no message

---
+++ app/Console/Commands/CytxUserPhoneChange.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class CytxUserPhoneChange extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:CytxUserPhoneChange';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $list = [
+//            ['18198080359','13369423657'],
+//            ['18877762860','13684118715'],
+//            ['15945369583','18104537687'],
+//            ['15595182687','13995193090'],
+//            ['17752314931','13895002795'],
+//            ['13995370916','15378964931'],
+//            ['18260429996','13776046443'],
+        ];
+
+        DB::beginTransaction();
+        try {
+            foreach ($list as $p){
+                $check_p1 = User::query()->where('phone','=',$p[0])->first();
+
+                if (empty($check_p1)){
+                    continue;
+                }
+
+                $check_p2 = User::query()->where('phone','=',$p[1])->first();
+
+                if (empty($check_p2)){
+                    $check_p1->phone = $p[1];
+                    $check_p1->save();
+                }else{
+                    $check_p2->phone = $p[0].'x';
+                    $check_p2->save();
+
+                    $check_p1->phone = $p[1];
+                    $check_p1->save();
+
+                    $check_p2->phone = $p[0];
+                    $check_p2->save();
+                }
+
+            }
+            DB::commit();
+            echo '成功';
+        } catch (\Exception $e) {
+            DB::rollBack();
+            echo '失败';
+        }
+    }
+}
