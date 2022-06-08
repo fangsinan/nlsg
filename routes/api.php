25f562afb555727b32806d358d1d8813681fa648
fangsinan <15650701817@163.com>
2022-06-08 17:18:36 +0800

    STS 鉴权

--- routes/api.php
+++ routes/api.php
@@ -629,7 +629,7 @@ Route::group(['namespace' => 'Api\V5', 'prefix' => 'v5'], function () {
     // 收藏
     Route::any('collection/list', 'CollectionController@list');//->middleware('replaceNull');
     Route::any('config/share', 'ConfigController@share');//->middleware('replaceNull');
-
+    Route::any('config/get_ali_proof', 'ConfigController@getAliProof');
     //微信url link&scheme
     Route::get('wechat_tools/get_url_link', 'WeChatToolsController@getUrlLink');
     /**     5.0 API  END    ***/
