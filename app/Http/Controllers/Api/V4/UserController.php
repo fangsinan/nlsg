2e167df0b9368debcbcfbf028239cb3fb99e78de
fangsinan <15650701817@163.com>
2022-06-17 14:07:37 +0800

    训练营

--- app/Http/Controllers/Api/V4/UserController.php
+++ app/Http/Controllers/Api/V4/UserController.php
@@ -809,6 +809,7 @@ class UserController extends Controller
             $val['works_name'] = '';
             $val['works_cover_img'] = '';
             $val['worksInfo_name'] = '';
+            $val['column_can_h5'] = 0;
 
 
             if ($val['relation_type'] == 1 or $val['relation_type'] == 2 or $val['relation_type'] == 5) {
@@ -816,6 +817,7 @@ class UserController extends Controller
                 $column = Column::where(['id'=>$val['relation_id'],'status'=>1])->first();
                 $val['column_name'] = $column['name'] ?? '';
                 $val['column_cover_img'] = $column['cover_pic'] ?? '';
+                $val['column_can_h5'] = $column['can_h5'] ?? 0;
             }
             if ($val['relation_type'] == 3 or $val['relation_type'] == 4) {
 //                $works = Works::find($val['relation_id']);
