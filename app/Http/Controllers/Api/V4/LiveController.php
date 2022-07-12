ce66f5d57f976b7e3242040331761f09dc2d4b79
w <han313500313@126.com>
2022-07-12 15:33:44 +0800

    no message

--- app/Http/Controllers/Api/V4/LiveController.php
+++ app/Http/Controllers/Api/V4/LiveController.php
@@ -1011,7 +1011,7 @@ class LiveController extends Controller
         }
         $list = LiveInfo::with([
             'user:id,nickname,headimg,intro,honor',
-            'live:id,title,price,cover_img,content,twitter_money,is_free,playback_price,is_show,helper,msg,describe,can_push,password,is_finish,virtual_online_num,classify',
+            'live:id,title,price,cover_img,content,twitter_money,is_free,playback_price,is_show,helper,msg,describe,can_push,password,is_finish,virtual_online_num,classify,welcome',
             'live.livePoster'=>function($q){
                 $q->where('status','=',1);
             }
