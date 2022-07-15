5385276188502151e4b6a3ca05db448a4224953d
w <han313500313@126.com>
2022-07-15 10:23:21 +0800

    396导入方法

--- app/Console/Commands/CytxUserPhoneChange.php
+++ app/Console/Commands/CytxUserPhoneChange.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\PayRecordDetail;
 use App\Models\User;
 use App\Servers\VipServers;
 use Illuminate\Console\Command;
@@ -40,28 +41,133 @@ class CytxUserPhoneChange extends Command
      */
     public function handle()
     {
-//        if (true){
-//            $list = [
-//                '18624078563',
-//            ];
-//
-//            $vip_temp_res = [];
-//            $vs = new VipServers();
-//            foreach ($list as $v){
-//                $p = [
-//                    "flag"=>1,
-//                    "parent"=>'',
-//                    "phone"=>$v,
-//                    "send_money"=>0
-//                ];
-//                $vip_temp_res[] = $vs->createVip_1($p,1);
-//            }
-//
-//            dd($vip_temp_res);
-//        }
-        return 0;
 
-        $list = [
+        if (0) {
+            //根据w_temp_360表  批量添加396收益
+
+            /**
+             * 补全数据部分
+             */
+            if (0) {
+                $list = DB::table('w_temp_396')
+                    ->where('is_done', '=', 0)
+                    ->get();
+
+                if ($list->isEmpty()) {
+                    exit('没有数据');
+                }
+
+                $this_time_remark = '220715导入';
+
+                foreach ($list as $key => $value) {
+
+                    $update_data = [];
+
+                    //过滤掉ordernum中不是数字的字符
+                    if ($value->ordernum === null) {
+                        $update_data['ordernum'] = '';
+                    } else {
+                        $update_data['ordernum'] = preg_replace('/[^0-9]/', '', $value->ordernum);
+                    }
+
+                    $update_data['remark'] = $this_time_remark;
+
+                    if (empty($value->user_id)) {
+                        $check_user             = User::query()
+                            ->firstOrCreate([
+                                'phone' => (string)$value->phone,
+                            ], [
+                                'nickname' => substr_replace($value->phone, '****', 3, 4),
+                            ]);
+                        $update_data['user_id'] = $check_user->id;
+                    }
+
+                    if (empty($update_data)) {
+                        continue;
+                    }
+
+                    DB::table('w_temp_396')->where('id', '=', $value->id)->update($update_data);
+
+                }
+            }
+
+
+            /**
+             * 写入收益部分
+             */
+            if (1) {
+                $list = DB::table('w_temp_396')
+                    ->where('is_done', '=', 0)
+                    ->get();
+
+                if ($list->isEmpty()) {
+                    exit('没有数据');
+                }
+
+                $now = time();
+
+                foreach ($list as $v) {
+                    $temp_insert_data = [
+                        'type'     => 13,
+                        'ordernum' => $v->ordernum,
+                        'user_id'  => $v->user_id,
+                        'price'    => $v->price,
+                        'ctime'    => $now,
+                        'remark'   => $v->remark,
+                    ];
+
+
+                    DB::beginTransaction();
+
+                    $insert_res = PayRecordDetail::query()
+                        ->insert($temp_insert_data);
+                    if (!$insert_res) {
+                        DB::rollBack();
+                        continue;
+                    }
+
+                    $update_res = DB::table('w_temp_396')
+                        ->where('id', '=', $v->id)
+                        ->update(['is_done' => 1]);
+                    if (!$update_res) {
+                        DB::rollBack();
+                        continue;
+                    }
+
+                    DB::commit();
+                }
+
+            }
+
+
+        }
+
+
+        if (0) {
+            //批量开通360部分
+            $list = [
+                '18624078563',
+            ];
+
+            $vip_temp_res = [];
+            $vs           = new VipServers();
+            foreach ($list as $v) {
+                $p              = [
+                    "flag"       => 1,
+                    "parent"     => '',
+                    "phone"      => $v,
+                    "send_money" => 0
+                ];
+                $vip_temp_res[] = $vs->createVip_1($p, 1);
+            }
+
+            dd($vip_temp_res);
+        }
+
+
+        if (0) {
+            //创业天下号码更改
+            $list = [
 //            ['18198080359','13369423657'],
 //            ['18877762860','13684118715'],
 //            ['15945369583','18104537687'],
@@ -69,39 +175,42 @@ class CytxUserPhoneChange extends Command
 //            ['17752314931','13895002795'],
 //            ['13995370916','15378964931'],
 //            ['18260429996','13776046443'],
-        ];
+            ];
 
-        DB::beginTransaction();
-        try {
-            foreach ($list as $p){
-                $check_p1 = User::query()->where('phone','=',$p[0])->first();
+            DB::beginTransaction();
+            try {
+                foreach ($list as $p) {
+                    $check_p1 = User::query()->where('phone', '=', $p[0])->first();
 
-                if (empty($check_p1)){
-                    continue;
+                    if (empty($check_p1)) {
+                        continue;
+                    }
+
+                    $check_p2 = User::query()->where('phone', '=', $p[1])->first();
+
+                    if (empty($check_p2)) {
+                        $check_p1->phone = $p[1];
+                        $check_p1->save();
+                    } else {
+                        $check_p2->phone = $check_p2->id;
+                        $check_p2->save();
+
+                        $check_p1->phone = $p[1];
+                        $check_p1->save();
+
+                        $check_p2->phone = $p[0];
+                        $check_p2->save();
+                    }
+
                 }
-
-                $check_p2 = User::query()->where('phone','=',$p[1])->first();
-
-                if (empty($check_p2)){
-                    $check_p1->phone = $p[1];
-                    $check_p1->save();
-                }else{
-                    $check_p2->phone = $check_p2->id;
-                    $check_p2->save();
-
-                    $check_p1->phone = $p[1];
-                    $check_p1->save();
-
-                    $check_p2->phone = $p[0];
-                    $check_p2->save();
-                }
-
+                DB::commit();
+                echo '成功';
+            } catch (\Exception $e) {
+                DB::rollBack();
+                echo '失败';
             }
-            DB::commit();
-            echo '成功';
-        } catch (\Exception $e) {
-            DB::rollBack();
-            echo '失败';
         }
+
+
     }
 }
