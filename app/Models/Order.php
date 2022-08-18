de3c0959adaef52a545a8874808fd3fff00fc221
fangsinan <15650701817@163.com>
2022-08-18 16:31:31 +0800

    no message

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -27,8 +27,7 @@ class Order extends Base
         'id', 'live_num', 'pay_type', 'activity_tag', 'kun_said', 'refund_no', 'is_live_order_send',
         'ordernum', 'status', 'type', 'user_id', 'relation_id', 'cost_price', 'price', 'twitter_id', 'coupon_id', 'ip',
         'os_type', 'live_id', 'reward_type', 'reward', 'service_id', 'reward_num', 'pay_time', 'start_time', 'end_time',
-        'pay_price', 'city', 'vip_order_type', 'send_type', 'send_user_id', 'remark', 'sales_id', 'sales_bind_id','protect_user_id',
-
+        'pay_price', 'city', 'vip_order_type', 'send_type', 'send_user_id', 'remark', 'sales_id', 'sales_bind_id','protect_user_id','live_admin_id',
 
     ];
 
