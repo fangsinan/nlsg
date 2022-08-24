311a76b5fa3110c883e2a91c7acb1a725cafff2d
lampsgl50 <lampsgl50@163.com>
2022-08-24 14:24:56 +0800

    消息

--- app/Http/Controllers/Api/V5/MessageController.php
+++ app/Http/Controllers/Api/V5/MessageController.php
@@ -11,6 +11,7 @@ use App\Models\Like;
 use App\Models\Message\Message;
 use App\Models\Message\MessageUser;
 use App\Models\UserFollow;
+use App\Models\VipUser;
 use App\Models\WorksInfo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -65,11 +66,12 @@ class MessageController extends Controller
      */
     public function msg_commen_list(Request $request)
     {
+
         $user_id = $this->user['id'] ?? 2;
 
         //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
         $lists = MessageUser::query()
-            ->select(['id', 'send_user', 'receive_user', 'message_id', 'status', 'created_at'])
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
             ->with([
                 'message:id,type,title,message,action_id',
                 'send_user:id,nickname,headimg,is_author',
@@ -80,6 +82,8 @@ class MessageController extends Controller
             ->paginate()->toArray();
 
         foreach ($lists['data'] as &$items) {
+            //是不是360vip
+            $items['send_user']['is_vip']=VipUser::newVipInfo($v['user']['id']??0)['vip_id'] ?1:0;
 
             //格式化时间
             $items['created_at'] = History::DateTime($items['created_at']);
@@ -151,7 +155,7 @@ class MessageController extends Controller
 
         //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
         $items = MessageUser::query()
-            ->select(['id', 'send_user', 'receive_user', 'message_id', 'status', 'created_at'])
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
             ->with([
                 'message:id,type,title,message,action_id',
                 'send_user:id,nickname,headimg,is_author',
@@ -160,6 +164,9 @@ class MessageController extends Controller
             ->where('type', 0)
             ->where('receive_user', $user_id)
             ->first()->toArray();
+
+        //是不是360vip
+        $items['send_user']['is_vip']=VipUser::newVipInfo($v['user']['id']??0)['vip_id'] ?1:0;
 
         //格式化时间
         $items['created_at'] = History::DateTime($items['created_at']);
@@ -234,13 +241,72 @@ class MessageController extends Controller
      */
     public function msg_follow_list(Request $request)
     {
+
         $user_id = $this->user['id'] ?? 2;
 
         //9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
+        $lists=MessageUser::query()
+            ->select([
+                DB::raw('GROUP_CONCAT(id) as ids'),
+                DB::raw('count(*) as count'),
+                DB::raw("date_format(created_at,'%Y-%m-%d') as time"),
+            ])->groupBy(DB::raw("date_format(created_at,'%Y-%m-%d')"))
+            ->where('type', 0)
+            ->where('receive_user', $user_id)
+            ->paginate()->toArray();
+
+        foreach ($lists['data'] as &$msg){
+
+            $ids_arr=explode(',',$msg['ids']);
+            $follow_lists = MessageUser::query()
+                ->select(['id', 'send_user','type', 'receive_user', 'message_id', 'status', 'created_at'])
+                ->with([
+                    'message:id,type,title,message,action_id',
+                    'send_user:id,nickname,headimg,is_author',
+                    'receive_user:id,nickname,headimg,is_author'
+                ])
+                ->whereIn('id', $ids_arr)
+                ->where('type', 0)
+                ->where('receive_user', $user_id)
+                ->get()->toArray();
+
+            foreach ($follow_lists as &$items) {
+                //是不是360vip
+                $items['send_user']['is_vip']=VipUser::newVipInfo($v['user']['id']??0)['vip_id'] ?1:0;
+
+                $items['created_at'] = History::DateTime($items['created_at']);
+                $items['is_follow'] = UserFollow::IsFollow($items['receive_user'], $items['send_user']);
+            }
+
+            $msg['follow_list']=$follow_lists;
+        }
+
+        return success($lists);
+    }
 
 
-        $follow_lists = MessageUser::query()
-            ->select(['id', 'send_user', 'receive_user', 'message_id', 'status', 'created_at'])
+    /**
+     * @api {get} /api/v5/message/msg_like_list 评论消息列表
+     * @apiName msg_like_list
+     * @apiVersion 1.0.0
+     * @apiGroup message
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+     * {
+     * "code": 200,
+     * "msg": "成功",
+     * "data": { }
+     * }
+     */
+    public function msg_like_list(Request $request)
+    {
+        $user_id = $this->user['id'] ?? 2;
+
+        //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
+
+        $lists = MessageUser::query()
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
             ->with([
                 'message:id,type,title,message,action_id',
                 'send_user:id,nickname,headimg,is_author',
@@ -248,15 +314,183 @@ class MessageController extends Controller
             ])
             ->where('type', 0)
             ->where('receive_user', $user_id)
-            ->get()->toArray();
+            ->paginate()->toArray();
 
-        foreach ($follow_lists as &$items) {
+        foreach ($lists['data'] as &$items) {
+
+            //是不是360vip
+            $items['send_user']['is_vip']=VipUser::newVipInfo($v['user']['id']??0)['vip_id'] ?1:0;
+
+            //格式化时间
             $items['created_at'] = History::DateTime($items['created_at']);
-            $items['is_follow'] = UserFollow::IsFollow($items['receive_user'], $items['send_user']);
+
+            //获取点赞
+            $items['like']=Like::query()->where('id',1083)->first();
+
+            //获取回复
+            $CommentReply = CommentReply::query()
+                ->with([
+                    'from_user:id,nickname,headimg,is_author',
+                    'to_user:id,nickname,headimg,is_author'
+                ])
+                ->where('id', 2397)->first();
+            if (!$CommentReply) {
+                return $this->error(0, '参数错误');
+            }
+            $items['comment_reply'] = $CommentReply;
+
+            //获取评论
+            $comment_id = $CommentReply->comment_id;
+            $Comment = Comment::query()
+                ->with([
+                    'user:id,nickname,headimg,is_author', 'quote:id,pid,content', 'attach:id,relation_id,img',
+                ])
+                ->select('id', 'pid', 'user_id', 'relation_id', 'info_id', 'content', 'forward_num',
+                    'share_num', 'like_num', 'reply_num', 'created_at', 'is_quality', 'is_top')
+                ->where('id', $comment_id)
+                ->whereIn('type', [1, 2, 6]) //类型 1.专栏 2.讲座 3.听书 4.精品课 5 百科 6训练营  7短视频
+                ->first();
+            if (!$Comment) {
+                return $this->error(0, '参数错误');
+            }
+            $items['comment'] = $Comment;
+
+            //获取训练营或专栏
+            $Column = Column::query()->where('id', $Comment->relation_id)
+                ->select(['id', 'name', 'title', 'index_pic', 'cover_pic', 'details_pic'])->first();
+            $items['column'] = $Column;
+
+            //获取章节
+            if ($Comment->info_id) {
+                $items['works_info'] = WorksInfo::query()->where(['id' => $Comment->info_id])->select('id', 'title')->first();
+            }
         }
 
-        return success($follow_lists);
+        return success($lists);
+    }
 
+    /**
+     * @api {get} /api/v5/message/msg_work_new_list 内容上新消息
+     * @apiName msg_work_new_list
+     * @apiVersion 1.0.0
+     * @apiGroup message
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+     * {
+     * "code": 200,
+     * "msg": "成功",
+     * "data": { }
+     * }
+     */
+    public function msg_work_new_list(Request $request)
+    {
+        $user_id = $this->user['id'] ?? 2;
+
+        //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
+
+        $lists = MessageUser::query()
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
+            ->with([
+                'message:id,type,title,message,action_id,relation_type,relation_id,relation_info_id',
+            ])
+            ->where('type', 0)
+            ->where('receive_user', $user_id)
+            ->paginate()->toArray();
+
+        foreach ($lists['data'] as &$items) {
+            //格式化时间
+            $items['created_at'] = History::DateTime($items['created_at']);
+
+        }
+
+        return success($lists);
+    }
+
+
+    /**
+     * @api {get} /api/v5/message/msg_system_list 系统消息列表
+     * @apiName msg_system_list
+     * @apiVersion 1.0.0
+     * @apiGroup message
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+     * {
+     * "code": 200,
+     * "msg": "成功",
+     * "data": { }
+     * }
+     */
+    public function msg_system_list(Request $request)
+    {
+        $user_id = $this->user['id'] ?? 2;
+
+        //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
+
+        $lists = MessageUser::query()
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
+            ->with([
+                'message:id,type,title,message,action_id,relation_type,relation_id,relation_info_id',
+            ])
+            ->where('type', 0)
+            ->where('receive_user', $user_id)
+            ->paginate()->toArray();
+
+        foreach ($lists['data'] as &$items) {
+            //格式化时间
+            $items['created_at'] = History::DateTime($items['created_at']);
+
+        }
+
+        return success($lists);
+    }
+
+    /**
+     * @api {get} /api/v5/message/msg_profit_list 收益消息列表
+     * @apiName msg_profit_list
+     * @apiVersion 1.0.0
+     * @apiGroup message
+     *
+     * @apiSuccess {string} result json
+     * @apiSuccessExample Success-Response:
+     * {
+     * "code": 200,
+     * "msg": "成功",
+     * "data": { }
+     * }
+     */
+    public function msg_profit_list(Request $request)
+    {
+
+        $user_id = $this->user['id'] ?? 2;
+
+        //type :9=评论 22=关注 (以真实关注的数据为准) 11=点赞  1=系统消息 4=内容上新  12=收益
+
+        $lists = MessageUser::query()
+            ->select(['id', 'send_user', 'type','receive_user', 'message_id', 'status', 'created_at'])
+            ->with([
+                'message:id,type,title,message,action_id,relation_type,relation_id,relation_info_id',
+            ])
+            ->where('type', 0)
+            ->where('receive_user', $user_id)
+            ->paginate()->toArray();
+
+        foreach ($lists['data'] as &$items) {
+
+            $msg_arr=json_decode($items['message']['message'],true);
+
+            if(is_array($msg_arr)){
+                $items['message']['message']=$msg_arr;
+            }else{
+                $items['message']['message']=['content'=>'内容','source'=>'奖励来源','type'=>'奖励类型','amount'=>'奖励数量','time'=>'奖励时间'];
+            }
+
+            //格式化时间
+            $items['created_at'] = History::DateTime($items['created_at']);
+        }
+
+        return success($lists);
     }
 
 }
