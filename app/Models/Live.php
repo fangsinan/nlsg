4504103d5eeb3159fc4382343cc7a8865b612d42
w <han313500313@126.com>
2022-09-22 10:11:43 +0800

    no message

--- app/Models/Live.php
+++ app/Models/Live.php
@@ -9,7 +9,7 @@ class Live extends Base
     protected $fillable = ['user_id', 'cover_img', 'title', 'describe', 'price', 'twitter_money', 'begin_at',
         'end_at', 'helper','is_free','content','need_virtual','need_virtual_num','steam_end_time','bgp_id',
                            'steam_begin_time','classify','valid_time_range','cover_vertical_img',
-                           'is_test','pre_push_time',
+                           'is_test','pre_push_time','service_type',
     ];
     public function getIndexLive($ids)
     {
