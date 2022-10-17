6d0053a4ce7c1f67cbacac4e1c9ec60173dc9b01
fangsinan <15650701817@163.com>
2022-10-17 16:23:36 +0800

    no message

---
+++ app/Models/TalkList.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Models;
+
+class TalkList extends Base
+{
+    protected $table = 'nlsg_talk_list';
+
+    protected $fillable = [
+        'talk_id', 'type','user_id','admin_id','content'
+    ];
+
+
+
+    public function user()
+    {
+        return  $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    /**
+     * GetListByUserId  获取答案
+     *
+     * @param  $uid
+     *
+     * @return array list: 答案列表  is_show_qr: 是否需显示二维码
+     */
+    public static function GetListByUserId($uid): array
+    {
+        $list = self::with(
+            "user:id,nickname,headimg"
+        )->select("content","user_id",'admin_id','type')
+            ->where("user_id",$uid)
+            ->where("status",1)->get();
+        if(empty($list)) return [];
+
+        $list = $list->toArray();
+
+        foreach($list as &$val){
+            //查看客服
+            if($val['type'] == 2){
+                // BackendUser::select("username")->find($val['admin_id']);
+                $val['user']['id'] = $val['admin_id'];
+                $val['user']['nickname'] = "客服";
+                $val['user']['headimg'] = "image/202009/13f952e04c720a550193e5655534be86.jpg";
+
+            }
+        }
+
+        return $list;
+    }
+
+}
