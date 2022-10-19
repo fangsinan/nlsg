0074a6e697521bfedf1f69c977c5afad478517a8
w <han313500313@126.com>
2022-10-19 14:49:40 +0800

    no message

--- app/Models/TalkRemark.php
+++ app/Models/TalkRemark.php
@@ -4,11 +4,18 @@
 namespace App\Models;
 
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class TalkRemark extends Base
 {
     protected $table = 'nlsg_talk_remark';
 
     protected $fillable = [
-        'talk_id','content','admin_id'
+        'talk_id', 'content', 'admin_id'
     ];
+
+    public function adminInfo(): HasOne
+    {
+        return $this->hasOne(BackendUser::class, 'id', 'admin_id');
+    }
 }
