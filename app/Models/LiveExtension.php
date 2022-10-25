fb47f85139ccca982320eee3cdffb2c8b855a544
zouxuguang <>
2022-10-25 15:14:25 +0800

    no message

---
+++ app/Models/LiveExtension.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+
+class LiveExtension extends Base
+{
+
+    protected $table = 'nlsg_live_extension';
+
+
+}
