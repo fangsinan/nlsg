b72a0d9d87a3bfd19c5d50ddf5b72acc0716344b
fangsinan <15650701817@163.com>
2022-11-01 10:25:57 +0800

    no message

---
+++ app/Models/OrderZero.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Description of MallCategory
+ *
+ * @author wangxh
+ */
+class OrderZero extends Base
+{
+
+    protected $table = 'nlsg_order_zero';
+
+    protected $fillable = [
+        'id', 'relation_id','user_id','status','pay_time', 'start_time', 'end_time',
+        'ordernum', 'ip', 'pay_type',  'os_type','twitter_id', 'remark', 'live_admin_id',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function twitter()
+    {
+        return $this->belongsTo(User::class, 'twitter_id', 'id');
+    }
+}
