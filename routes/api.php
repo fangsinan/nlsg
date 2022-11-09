6eafdd495f0ed4524a49655c498056fec45230e4
fangsinan <15650701817@163.com>
2022-11-09 13:43:11 +0800

    auth

--- routes/api.php
+++ routes/api.php
@@ -241,6 +241,7 @@ Route::group(['namespace' => 'Api\V4', 'prefix' => 'v4'], function () {
     Route::get('user/invitation_record', 'UserController@invitationRecord');//邀请记录
 
 
+    Route::any('auth/user_collect', 'AuthController@zeroPhoneCollect');
     Route::post('auth/check_wx', 'AuthController@checkWx');
     Route::post('auth/bind', 'AuthController@bind');
     Route::post('auth/channel_bind', 'AuthController@channel_bind');
