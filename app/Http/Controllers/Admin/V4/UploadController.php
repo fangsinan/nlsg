ac0f1661e36f43326e8ca4b821ca3b47602a4a44
nlsg <15650701817@163.com>
2022-11-25 19:50:31 +0800

    test  upload

--- app/Http/Controllers/Admin/V4/UploadController.php
+++ app/Http/Controllers/Admin/V4/UploadController.php
@@ -17,9 +17,9 @@ class UploadController extends ControllerBackend
     {
         $type = $request->get('type') ?? 'video';
         if ($type == 'video') {
-            $data = $this->upload(855795);
+            $data = $this->upload(934426);
         } elseif ($type == 'audio') {
-            $data = $this->upload(855794, 0);
+            $data = $this->upload(934427, 0);
         } elseif ($type == 'short_video') {
             $data = $this->upload(867416, 0);
         }
@@ -127,5 +127,226 @@ class UploadController extends ControllerBackend
         }
         return success();
     }
+
+
+
+
+//    public static function editVideos()
+//    {
+//        dd('完了');
+//        $video_ids = DB::table('nlsg_works_info_pro')->where([
+//            "type"   => 2,
+//            "is_end"   => 1,
+//        ])->limit(1000)->pluck("new_video_id")->toArray();
+//
+//
+//        // 移除视频至删除分类
+//        if(empty($video_ids) ){
+//            dd('完了');
+//        }
+//
+//        if(!is_array($video_ids)){
+//            $video_ids = [$video_ids];
+//        }
+//
+//        //多个处理
+//        $uri = 'vod.tencentcloudapi.com';
+//        $secretKey = config('env.TENCENT_SECRETKEY');
+//        foreach($video_ids as $video_id){
+//
+//            //加密
+//            $rand   = rand (100, 10000000); //9031868223070871051
+//            $time   = time ();
+//            $data_key = [
+//                'Action' => 'ModifyMediaInfo',
+//                'Version' => '2018-07-17',
+//                'Language' => "zh-CN",
+//                'FileId' => $video_id,
+//                'ClassId' => '934427',
+//                'Region' => "",
+//                'SecretId' => config('env.TENCENT_SECRETID'),
+//                'Timestamp' => $time,
+//                'Nonce' => $rand,
+//                // 'SignatureMethod' => 'HmacSHA256',
+//            ];
+//
+//
+//            ksort ($data_key); //排序
+//            // 计算签名
+//            $srcStr    = "GET".$uri."/?" . http_build_query ($data_key);
+//            $signature = base64_encode (hash_hmac ('sha256', $srcStr, $secretKey, true)); //SHA1  sha256
+//            $signature = base64_encode(hash_hmac("sha1", $srcStr, $secretKey, true));
+//            $data_key['Signature'] = $signature;
+//            ksort ($data_key); //排序
+//            //拉取转码成功信息
+//            $url = "https://vod.tencentcloudapi.com/?".http_build_query ($data_key); //?Action=PullEvent&COMMON_PARAMS
+//            $info = ImClient::curlGet ($url);  //post
+//            dump($info);
+//            // $info = json_decode($info,true);
+//            DB::table('nlsg_works_info_pro')->where([
+//                'new_video_id'   => $video_id, //会员id
+//            ])->update(['is_end' => 2,]);
+////            dd("end");
+//
+//        }
+//
+//        return success();
+//    }
+//
+//
+//
+//
+//    public static function delVideos()
+//    {
+//        dd(1);
+//        $video_ids = DB::table('nlsg_works_info_pro')->where([
+//            "type"   => 2,
+//            "is_end"   => 1,
+//        ])->limit(2000)->pluck("new_video_id")->toArray();
+//
+//
+//        // 移除视频至删除分类
+//        if(empty($video_ids) ){
+//            dd('完了');
+//        }
+//
+//        if(!is_array($video_ids)){
+//            $video_ids = [$video_ids];
+//        }
+//
+//        //多个处理
+//        $uri = 'vod.tencentcloudapi.com';
+//        $secretKey = config('env.TENCENT_SECRETKEY');
+//        foreach($video_ids as $video_id){
+//
+//            //加密
+//            $rand   = rand (100, 10000000); //9031868223070871051
+//            $time   = time ();
+//            $data_key = [
+//                'Action' => 'DeleteMedia',
+//                'Version' => '2018-07-17',
+//                'Language' => "zh-CN",
+//                'FileId' => $video_id,
+//                'Region' => "",
+//                'SecretId' => config('env.TENCENT_SECRETID'),
+//                'Timestamp' => $time,
+//                'Nonce' => $rand,
+//                'DeleteParts.0.Type' => "TranscodeFiles",
+//            ];
+//
+//            ksort ($data_key); //排序
+//            // 计算签名
+//            $srcStr    = "GET".$uri."/?" . http_build_query ($data_key);
+//            $signature = base64_encode (hash_hmac ('sha256', $srcStr, $secretKey, true)); //SHA1  sha256
+//            $signature = base64_encode(hash_hmac("sha1", $srcStr, $secretKey, true));
+//            $data_key['Signature'] = $signature;
+//            ksort ($data_key); //排序
+//            //拉取转码成功信息
+//            $url = "https://vod.tencentcloudapi.com/?".http_build_query ($data_key); //?Action=PullEvent&COMMON_PARAMS
+//            $info = ImClient::curlGet ($url);  //post
+//            dump(($info));
+//            // $info = json_decode($info,true);
+//            DB::table('nlsg_works_info_pro')->where([
+//                'new_video_id'   => $video_id, //会员id
+//            ])->update(['is_end' => 2,]);
+//
+//        }
+//
+//        return success();
+//    }
+//
+//
+//
+//
+//
+//
+//
+//
+//    public static function delVideosInClassId()
+//    {
+//        dd(1);
+//
+//
+//        //多个处理
+//        $uri = 'vod.tencentcloudapi.com';
+//        $secretKey = config('env.TENCENT_SECRETKEY');
+//        //加密
+//        $rand   = rand (100, 10000000); //9031868223070871051
+//        $time   = time ();
+//        $data_key = [
+//            'Action' => 'SearchMedia',
+//            'Version' => '2018-07-17',
+//            'Language' => "zh-CN",
+//            'Region' => "",
+//            'Limit' => "1000",
+//            'SecretId' => config('env.TENCENT_SECRETID'),
+//            'Timestamp' => $time,
+//            'Nonce' => $rand,
+//            'ClassIds.0' => "855795",
+//        ];
+//
+//        ksort ($data_key); //排序
+//        // 计算签名
+//        $srcStr    = "GET".$uri."/?" . http_build_query ($data_key);
+//        $signature = base64_encode (hash_hmac ('sha256', $srcStr, $secretKey, true)); //SHA1  sha256
+//        $signature = base64_encode(hash_hmac("sha1", $srcStr, $secretKey, true));
+//        $data_key['Signature'] = $signature;
+//        ksort ($data_key); //排序
+//        //拉取转码成功信息
+//        $url = "https://vod.tencentcloudapi.com/?".http_build_query ($data_key); //?Action=PullEvent&COMMON_PARAMS
+//        $info = ImClient::curlGet ($url);  //post
+//
+//
+//        $info = (json_decode($info,true));
+//
+//
+//        $video_ids = array_column($info['Response']['MediaInfoSet'], "FileId");
+//dump($video_ids);
+//        // 移除视频至删除分类
+//        if(empty($video_ids) ){
+//            dd('完了');
+//        }
+//
+//        if(!is_array($video_ids)){
+//            $video_ids = [$video_ids];
+//        }
+//
+//
+//        foreach($video_ids as $video_id){
+//
+//            //加密
+//            $rand   = rand (100, 10000000); //9031868223070871051
+//            $time   = time ();
+//            $data_key = [
+//                'Action' => 'DeleteMedia',
+//                'Version' => '2018-07-17',
+//                'Language' => "zh-CN",
+//                'FileId' => $video_id,
+//                'Region' => "",
+//                'SecretId' => config('env.TENCENT_SECRETID'),
+//                'Timestamp' => $time,
+//                'Nonce' => $rand,
+//            ];
+//
+//            ksort ($data_key); //排序
+//            // 计算签名
+//            $srcStr    = "GET".$uri."/?" . http_build_query ($data_key);
+//            $signature = base64_encode (hash_hmac ('sha256', $srcStr, $secretKey, true)); //SHA1  sha256
+//            $signature = base64_encode(hash_hmac("sha1", $srcStr, $secretKey, true));
+//            $data_key['Signature'] = $signature;
+//            ksort ($data_key); //排序
+//            //拉取转码成功信息
+//            $url = "https://vod.tencentcloudapi.com/?".http_build_query ($data_key); //?Action=PullEvent&COMMON_PARAMS
+//            $info = ImClient::curlGet ($url);  //post
+////            dump(($info));/
+//            // $info = json_decode($info,true);
+//            DB::table('nlsg_works_info_pro')->where([
+//                'new_video_id'   => $video_id, //会员id
+//            ])->update(['is_end' => 2,]);
+//
+//        }
+//
+//        return success();
+//    }
 
 }
