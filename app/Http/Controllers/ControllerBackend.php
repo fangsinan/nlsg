ba25f01d0bc96b828f9f22f843a87ee310d5e9c2
wxh <>
2022-11-30 20:11:00 +0800

    no message

--- app/Http/Controllers/ControllerBackend.php
+++ app/Http/Controllers/ControllerBackend.php
@@ -41,7 +41,7 @@ class ControllerBackend extends BaseController
         $url_2 = array_slice($url_2, -2);
         $url_2 = '/' . trim(implode('/', $url_2), '/');
 
-        if ($url_2 !== '/auth/login') {
+        if ($url_2 !== '/auth/login' || $url_2 !== '/auth/captcha') {
             $cache_token = BackendUserToken::getToken($this->user['id']);
             if ($cache_token) {
                 $header_token = $request->header('authorization');
