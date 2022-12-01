d2a100586271cb6fd94158de6d744361f78883dd
wxh <>
2022-12-01 19:43:47 +0800

    no message

--- app/Servers/V5/BackendUserToken.php
+++ app/Servers/V5/BackendUserToken.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Cache;
 class BackendUserToken
 {
     const KeyPre = 'AdminToken:';
-    const TokenLife = 300;//正式半小时
+    const TokenLife = 1800;//正式半小时
 
     public static function setToken(int $admin_id, string $token)
     {
