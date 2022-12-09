304f6d9c27167194f4a393e05cf193aeb2912896
wxh <>
2022-12-09 11:32:17 +0800

    xet

---
+++ app/Servers/V5/XiaoETongServers.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Servers\V5;
+
+
+class XiaoETongServers
+{
+    public function vipList($params, $admin)
+    {
+        return [__LINE__];
+    }
+
+    public function vipAdd($params, $admin)
+    {
+        return [__LINE__];
+    }
+
+    public function vipBindUser($params, $admin)
+    {
+        return [__LINE__];
+    }
+
+    public function vipInfo($params, $admin)
+    {
+        return [__LINE__];
+    }
+
+    public function orderList($params, $admin)
+    {
+        return [__LINE__];
+    }
+
+}
