7bef0da1a206842b94c04962fd738d5f45803150
fangsinan <15650701817@163.com>
2023-03-26 20:13:40 +0800

    no message

--- app/Http/Controllers/Api/V5/LiveController.php
+++ app/Http/Controllers/Api/V5/LiveController.php
@@ -176,9 +176,16 @@ class LiveController extends Controller
 			->where('begin_at','>', $day_time)
             ->where('status', 4)
             ->where('is_finish', 0)
-            ->where('is_del', 0)
-            ->where('app_project_type','=',APP_PROJECT_TYPE);
+            ->where('is_del', 0);
+            // ->where('app_project_type','=',APP_PROJECT_TYPE);
 
+        //  如果测试用户 可不做校验直接展示两个平台的直播  主播可以看自己的直播
+        if(empty($this->user['is_test_pay'])){
+            $query->where(function ($query)use($uid){
+                $query->where('app_project_type','=',APP_PROJECT_TYPE)
+                    ->Orwhere('user_id', $uid);;
+            });
+        }
             // 不查询测试直播的情况下
             // 需要查询当前用户是否管理员  单独查询管理员的
             if($is_test == 0 && !empty($this->user['phone'])){
