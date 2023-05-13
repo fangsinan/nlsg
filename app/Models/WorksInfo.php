928cdabcf561551e627fcb968c43b9defc1f393e
fangsinan <15650701817@163.com>
2023-05-13 17:14:07 +0800

    no message

--- app/Models/WorksInfo.php
+++ app/Models/WorksInfo.php
@@ -964,8 +964,7 @@ class WorksInfo extends Base
     {
         // return $url;
         $ex_times = explode(":",$duration);
-        // $time_v = $ex_times[0]*60 + $ex_times[1]+3600; //强制增加1小时观看失效，app老出现黑屏
-        $time_v = $ex_times[0]*60 + $ex_times[1];//+3600;
+        $time_v = $ex_times[0]*60 + $ex_times[1]+3600; //强制增加1小时观看失效，app老出现黑屏
 
         $url = str_replace("http://1308168117.vod2.myqcloud.com/",
             "https://vod.cloud.nlsgapp.com/",$url);
