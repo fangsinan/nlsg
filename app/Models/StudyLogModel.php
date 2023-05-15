0c03f30d5af3b79f6edff967a80f9472698f3def
lampsgl50 <lampsgl50@163.com>
2023-05-15 14:57:20 +0800

    训练营

---
+++ app/Models/StudyLogModel.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Models;
+
+
+use App\Models\Base;
+
+class StudyLogModel extends Base
+{
+    const DB_TABLE = 'nlsg_study_log';
+    protected $table = 'nlsg_study_log';
+
+}
