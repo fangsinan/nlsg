f21acae6ffddac0b1575edf5066791f601ed998c
w <wxh>
2023-05-17 19:39:57 +0800

    手机归属地信息查询

---
+++ app/Console/Commands/GetPhoneRegion.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Servers\UserRegionServers;
+use Illuminate\Console\Command;
+
+class GetPhoneRegion extends Command
+{
+
+    protected $signature = 'command:GetPhoneRegion  {type}';
+
+    protected $description = 'Command description';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $type = $this->argument('type')??'';
+
+        $s = new UserRegionServers();
+        if ($type === '1'){
+            $s->toAddList();
+        }else{
+            $s->toRun();
+        }
+        return 0;
+    }
+}
